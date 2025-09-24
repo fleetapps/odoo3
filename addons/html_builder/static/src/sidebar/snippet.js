f1c4934278e72055906c6ca64df3c18839bd73eb
Robin Lejeune (role) <role@odoo.com>
2025-09-24 14:52:39 +0200

    [IMP] html_builder, website: use arrow keys to navigate in tab panes
    The tab panes of the AddSnippetDialog and the AddPageDialog, as well as
    the snippets blocks in the sidebar, would benefit from a proper keyboard
    navigation. Since [9ae02d8], the snippets and page layouts are tabable.
    But when there are many snippets or layouts, an approach using the
    arrows would be more practical and intuitive, as it uses a masonry
    layout.
    
    The logic makes sure to handle both LTR and RTL languages.
    
    [9ae02d8]: https://github.com/odoo/odoo/commit/9ae02d80894d4043e49d8e2cad068f8018e6f113
    
    task-5384672

--- addons/html_builder/static/src/sidebar/snippet.js
+++ addons/html_builder/static/src/sidebar/snippet.js
@@ -1,5 +1,7 @@
 import { Image } from "@html_builder/core/img";
+import { handleMatrixKeyNavigation } from "@html_builder/utils/backend_utils";
 import { Component } from "@odoo/owl";
+import { getActiveHotkey } from "@web/core/hotkeys/hotkey_service";
 
 export class Snippet extends Component {
     static template = "html_builder.Snippet";
@@ -21,6 +23,18 @@ export class Snippet extends Component {
                 .querySelector(".o_install_btn")
                 .classList.toggle("visually-hidden-focusable", ev.type !== "mouseover");
         }
+    }
+
+    onBtnKeydown(ev) {
+        const hotkey = getActiveHotkey(ev);
+        if (hotkey === "enter" || hotkey === "space") {
+            this.props.onClickHandler(ev);
+        }
+        handleMatrixKeyNavigation(ev, {
+            containerEl: ev.currentTarget.closest(".o_snippets_container_body"),
+            focusedItemSelector: ".o_snippet",
+            focusableElSelector: "button",
+        });
     }
 
     onClickInstall() {
