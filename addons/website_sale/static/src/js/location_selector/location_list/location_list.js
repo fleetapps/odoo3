e4c8cdffc313511cc88f0721b750e70bdf31015a
Mohammad Abdulmoneim (abdu) <abdu@odoo.com>
2025-11-24 14:14:56 +0100

    [MOV] delivery->website_sale: location_selector
    This commit moves all the code related to pickup points feature from
    `delivery` module to `website_sale`. This is because there's no need to
    have the feature in `delivery`, since all delivery methods that
    currently support this feature depend on `website_sale`, namely:
    Sendcloud, FedEx and Click&Collect.
    
    Task-3645144

--- addons/website_sale/static/src/js/location_selector/location_list/location_list.js
+++ addons/website_sale/static/src/js/location_selector/location_list/location_list.js
@@ -1,10 +1,10 @@
 import { useLayoutEffect } from "@web/owl2/utils";
-import { Location } from '@delivery/js/location_selector/location/location';
+import { Location } from '@website_sale/js/location_selector/location/location';
 import { Component, onMounted } from '@odoo/owl';
 
 export class LocationList extends Component {
     static components = { Location };
-    static template = 'delivery.locationSelector.locationList';
+    static template = 'website_sale.locationSelector.locationList';
     static props = {
         locations: {
             type: Array,
@@ -24,15 +24,15 @@ export class LocationList extends Component {
                     street: String,
                     city: String,
                     zip_code: String,
-                    state: { type: String, optional: true},
+                    state: { type: String, optional: true },
                     country_code: String,
-                    additional_data: { type: Object, optional: true},
+                    additional_data: { type: Object, optional: true },
                     latitude: String,
                     longitude: String,
                 }
             },
         },
-        selectedLocationId: [String, {value: false}],
+        selectedLocationId: [String, { value: false }],
         setSelectedLocation: Function,
         validateSelection: Function,
     };
