27f29819064ffb52738fa5fa2b67b4561350b356
rusp-odoo <rusp@odoo.com>
2025-12-04 17:35:37 +0530

    [IMP] website_event: add dynamic carousel snippet for events
    Add a new dynamic carousel snippet that displays events in a
    carousel.
    
    To use:
    Edit mode -> Blocks -> Events -> Select one with "Dynamic Carousel"
    label
    
    task-5090459

--- addons/website_event/static/src/snippets/s_events/events.js
+++ addons/website_event/static/src/snippets/s_events/events.js
@@ -1,33 +1,23 @@
 import { DynamicSnippet } from "@website/snippets/s_dynamic_snippet/dynamic_snippet";
 import { registry } from "@web/core/registry";
 
-import { groupBy } from "@web/core/utils/arrays";
+import { EventsMixin } from "./events_mixin";
 
-export class Events extends DynamicSnippet {
+const EventsBase = EventsMixin(DynamicSnippet);
+
+export class Events extends EventsBase {
     // While the selector has 'upcoming_snippet' in its name, it now has a filter
     // option to include ongoing events. The name is kept for backward compatibility.
     static selector = ".s_event_upcoming_snippet";
-
-    /**
-     * @override
-     */
-    getSearchDomain() {
-        let searchDomain = super.getSearchDomain(...arguments);
-        const filterByTagIds = this.el.dataset.filterByTagIds;
-        if (filterByTagIds) {
-            let tagGroupedByCategory = groupBy(JSON.parse(filterByTagIds), "category_id");
-            for (const category in tagGroupedByCategory) {
-                searchDomain = searchDomain.concat(
-                    [["tag_ids", "in", tagGroupedByCategory[category].map(e => e.id)]]);
-            }
-        }
-        return searchDomain;
-    }
 }
 
 registry
     .category("public.interactions")
     .add("website_event.events", Events);
+
+registry.category("public.interactions.edit").add("website_event.events_base", {
+    Interaction: EventsBase,
+});
 
 registry
     .category("public.interactions.edit")
---
+++ addons/website_event/static/src/website_builder/dynamic_snippet_carousel_events_option.js
@@ -0,0 +1,6 @@
+import { DynamicSnippetEventsOption } from "./dynamic_snippet_events_option";
+
+export class DynamicSnippetCarouselEventsOption extends DynamicSnippetEventsOption {
+    static template = "website_event.DynamicSnippetCarouselEventsOption";
+    static selector = ".s_events_carousel";
+}
