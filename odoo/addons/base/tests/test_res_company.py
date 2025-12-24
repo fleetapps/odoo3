ab0f9d23adaa4b5f5b6dd94d904ae8fe1a6fd2ce
guva-odoo <guva@odoo.com>
2025-12-24 14:04:37 +0000

    [FIX] base: constrains wrongly triggered on fiscal last day
    Having a parent company and a chid company selected, and changing both
    the last day and the last month of the fiscal year as the same time
    raises a ValidationError.
    
    This is because in this case, in the write we successively modify each changed
    delegated fields from root company to the branches. Then, when
    checking the constrains we loop through all delegated fields and check
    if the value of the branches are the same as the root company. This
    check triggers the error as all values are not set yet.
    
    By using a write on branches for all changed delegated fields instead of
    a simple assignation, the constrains check occurs once all the value
    have been updated.
    
    Steps:
    
    - Have a root company and a branch
    - Select both in company selector
    - Go to Accounting configuration
    - Change fiscalyear last month AND ast day at the same time
    - Save
    -> ValidationError in `_check_root_delegated_fields`
    
    opw-5431145
    
    X-original-commit: 40ba71e96e0a9189a443a207bec1486f08107a5d

--- odoo/addons/base/tests/test_res_company.py
+++ odoo/addons/base/tests/test_res_company.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from unittest.mock import patch
+
 from odoo.exceptions import ValidationError
-from odoo.tests.common import tagged, TransactionCase
+from odoo.tests.common import Command, tagged, TransactionCase
 
 
 @tagged('at_install', '-post_install')  # LEGACY at_install
@@ -55,6 +57,7 @@ class TestCompany(TransactionCase):
     def test_create_branch_with_default_parent_id(self):
         branch = self.env['res.company'].with_context(default_parent_id=self.env.company.id).create({'name': 'Branch Company'})
         self.assertFalse(branch.partner_id.parent_id)
+<<<<<<< 3af323d544ca7dfbc8dca961d431dc832a226616
 
     def test_correct_order_of_company(self):
         """Ensure that companies are correctly ordered in a hierarchy"""
@@ -75,3 +78,16 @@ class TestCompany(TransactionCase):
             [main_company.id, second_company.id],
             "Companies are not in the correct order",
         )
+||||||| 654f7c11fcffebe047f9d56ee11eea2939365e52
+=======
+
+    def test_write_company_root_delegated_field_names(self):
+        self.env['res.company'].with_context(default_parent_id=self.env.company.id).create({'name': 'foo'})
+        new_currency = self.env['res.currency'].create({
+            'name': 'AAA',
+            'symbol': 'AAA',
+            'rate_ids': [Command.create({'name': '2009-09-09', 'rate': 1})]
+        })
+        with patch('odoo.addons.base.models.res_company.ResCompany._get_company_root_delegated_field_names', return_value=["currency_id", "zip"]):
+            self.env.company.write({'currency_id': new_currency.id, 'zip': '12345'})
+>>>>>>> 3d924d1901a8ba0df06ea391d130d5a35bef0b49
