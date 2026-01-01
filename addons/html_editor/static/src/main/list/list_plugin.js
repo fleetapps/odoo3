758230ccc413612b415705d4b12c1ad38fbfd224
Deependra Solanki <deso@odoo.com>
2026-01-01 12:18:50 +0530

    [IMP] html_editor: apply format to block if completely selected
    When a paragraph-related element is fully selected, formatting commands
    (e.g. font-size, font-style, font-weight, etc.) are applied directly on the
    block element instead of new span.
    
    List items already behaved this way; this change aligns other block elements
    with the same behavior.
    
    Applying the format on the block simplifies the DOM structure and ensures
    consistent behavior when formatting entire blocks.
    
    task-5215987

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -31,8 +31,8 @@ import { _t } from "@web/core/l10n/translation";
 import { compareListTypes, createList, insertListAfter, isListItem } from "./utils";
 import { callbacksForCursorUpdate } from "@html_editor/utils/selection";
 import { withSequence } from "@html_editor/utils/resource";
-import { FONT_SIZE_CLASSES, getFontSizeOrClass, getHtmlStyle } from "@html_editor/utils/formatting";
-import { getTextColorOrClass, TEXT_CLASSES_REGEX } from "@html_editor/utils/color";
+import { FONT_SIZE_CLASSES, TEXT_STYLE_CLASSES, getHtmlStyle } from "@html_editor/utils/formatting";
+import { TEXT_CLASSES_REGEX } from "@html_editor/utils/color";
 import { baseContainerGlobalSelector } from "@html_editor/utils/base_container";
 import { ListSelector } from "./list_selector";
 import { composeToolbarButton } from "../toolbar/toolbar";
@@ -177,7 +177,7 @@ export class ListPlugin extends Plugin {
         on_step_added_handlers: this.updateToolbarButtons.bind(this),
         on_deleted_handlers: this.adjustListPaddingOnDelete.bind(this),
         on_will_insert_separator_handlers: this.exitList.bind(this),
-        on_will_format_selection_handlers: this.applyFormatToListItem.bind(this),
+        on_block_formatted_handlers: this.postFormatAppliedOnList.bind(this),
 
         /** Processors */
         normalize_processors: this.normalize.bind(this),
@@ -388,11 +388,29 @@ export class ListPlugin extends Plugin {
     baseContainerToList(baseContainer, mode) {
         const cursors = this.dependencies.selection.preserveSelection();
         const list = insertListAfter(this.document, baseContainer, mode, childNodes(baseContainer));
-        const textAlign = baseContainer.style.getPropertyValue("text-align");
-        if (textAlign) {
-            // Copy text-align style from base container to li.
-            list.firstElementChild.style.setProperty("text-align", textAlign);
-            baseContainer.style.removeProperty("text-align");
+        // Copy some styles from base container to li.
+        const propertiesToMove = [
+            "text-align",
+            "font-size",
+            "font-weight",
+            "font-style",
+            "text-decoration-line",
+            "font-family",
+            "color",
+        ];
+        for (const property of propertiesToMove) {
+            const value = baseContainer.style.getPropertyValue(property);
+            if (value) {
+                list.firstElementChild.style.setProperty(property, value);
+                removeStyle(baseContainer, property);
+            }
+        }
+        // Copy some classes from base container to li.
+        for (const className of [...baseContainer.classList]) {
+            if ([...FONT_SIZE_CLASSES, ...TEXT_STYLE_CLASSES].includes(className)) {
+                list.firstElementChild.classList.add(className);
+                removeClass(baseContainer, className);
+            }
         }
         this.dependencies.dom.copyAttributes(baseContainer, list);
         this.adjustListPadding(list);
@@ -745,37 +763,34 @@ export class ListPlugin extends Plugin {
         }
         // Preserve style properties
         const dir = li.getAttribute("dir") || ul.getAttribute("dir");
-        const textAlign = li.style.getPropertyValue("text-align");
-        const liColorStyle = getTextColorOrClass(li);
-        const liFontSizeStyle = getFontSizeOrClass(li);
-        const wrapChildren = (parent, tag) => {
-            const wrapper = this.document.createElement(tag);
-            wrapper.append(...parent.childNodes);
-            parent.replaceChildren(wrapper);
-            cursors.remapNode(parent, wrapper);
-            return wrapper;
-        };
         for (const block of blocksToMove) {
             // text direction
             if (dir && !block.getAttribute("dir")) {
                 block.setAttribute("dir", dir);
             }
-            // text alignment
-            if (textAlign && !block.style.getPropertyValue("text-align")) {
-                block.style.setProperty("text-align", textAlign);
+            // Copy some styles from li to base container.
+            const propertiesToMove = [
+                "text-align",
+                "font-size",
+                "font-weight",
+                "font-style",
+                "text-decoration-line",
+                "font-family",
+                "color",
+            ];
+            for (const property of propertiesToMove) {
+                const value = li.style.getPropertyValue(property);
+                if (value) {
+                    block.style.setProperty(property, value);
+                }
             }
-            // text color
-            if (liColorStyle) {
-                const font = wrapChildren(block, "font");
-                this.dependencies.color.colorElement(font, liColorStyle.value, "color");
-            }
-            // font-size
-            if (liFontSizeStyle && !isEmptyBlock(block)) {
-                const span = wrapChildren(block, "span");
-                if (liFontSizeStyle.type === "font-size") {
-                    span.style.fontSize = liFontSizeStyle.value;
-                } else if (liFontSizeStyle.type === "class") {
-                    span.classList.add(liFontSizeStyle.value);
+            // Copy some classes from li to base container.
+            for (const className of [...li.classList]) {
+                if (
+                    [...FONT_SIZE_CLASSES, ...TEXT_STYLE_CLASSES].includes(className) ||
+                    TEXT_CLASSES_REGEX.test(className)
+                ) {
+                    block.classList.add(className);
                 }
             }
         }
@@ -1168,80 +1183,25 @@ export class ListPlugin extends Plugin {
         cursors.restore();
     }
 
-    applyFormatToListItem(formatName, { formatProps, applyStyle } = {}) {
+    postFormatAppliedOnList(formattedBlocks, formatName, applyStyle) {
         if (!["setFontSizeClassName", "fontSize"].includes(formatName)) {
             return;
         }
-        this.dependencies.split.splitSelection();
-        const targetedNodes = this.dependencies.selection.getTargetedNodes();
-        const listItems = new Set(
-            targetedNodes.map((n) => closestElement(n, "li")).filter(Boolean)
-        );
-        if (!listItems.size) {
-            return false;
-        }
         const listsSet = new Set();
-        const cursors = this.dependencies.selection.preserveSelection();
-        for (const listItem of listItems) {
-            // Skip list items with block descendants other than base
-            // container or a list related elements or no font size formatting
-            // to remove.
-            const hasOnlyBaseBlocks = [...descendants(listItem)]
-                .filter(isBlock)
-                .every((n) => n.matches(`${baseContainerGlobalSelector}, ol, ul, li`));
-            const hasExistingFontSize =
-                FONT_SIZE_CLASSES.some((c) => listItem.classList.contains(c)) ||
-                listItem.style.fontSize;
-            if (!hasOnlyBaseBlocks || (!applyStyle && !hasExistingFontSize)) {
-                continue;
-            }
-
-            if (this.dependencies.selection.areNodeContentsFullySelected(listItem)) {
-                for (const node of [
-                    listItem,
-                    ...descendants(listItem).filter(
-                        (n) => isElement(n) && closestElement(n, "LI") === listItem
-                    ),
-                ]) {
-                    removeClass(node, ...FONT_SIZE_CLASSES, "o_default_font_size");
-                    if (node.style.fontSize) {
-                        node.style.fontSize = "";
-                    }
-                }
-
-                if (applyStyle) {
-                    if (formatName === "setFontSizeClassName") {
-                        listItem.classList.add(formatProps.className);
-                    } else if (formatName === "fontSize") {
-                        listItem.style.fontSize = formatProps.size;
-                    }
-                    const sublists = childNodes(listItem).filter(isListElement);
-                    for (const list of sublists) {
+        for (const node of formattedBlocks) {
+            if (isListItem(node)) {
+                const sublists = childNodes(node).filter(isListElement);
+                for (const list of sublists) {
+                    if (applyStyle) {
                         list.classList.add("o_default_font_size");
                     }
                 }
-            } else if (!applyStyle && hasExistingFontSize) {
-                const textNodes = targetedNodes.filter(
-                    (n) => isVisibleTextNode(n) && closestElement(n, "li") === listItem
-                );
-                // Remove inline font size from partial selection by
-                // wrapping in span with default font size.
-                for (const node of textNodes) {
-                    const span = this.document.createElement("span");
-                    span.classList.add("o_default_font_size");
-                    node.before(span);
-                    cursors.update(callbacksForCursorUpdate.before(node, span));
-                    span.append(node);
-                    cursors.update(callbacksForCursorUpdate.append(span, node));
-                }
+                listsSet.add(node.parentElement);
             }
-            listsSet.add(listItem.parentElement);
         }
-        cursors.restore();
         for (const list of listsSet) {
             this.adjustListPadding(list);
         }
-        return true;
     }
 
     /**
