941168e25c0700f2a698efef8aee5fdf59a7e985
shmn-odoo <shmn@odoo.com>
2026-01-01 18:20:08 +0530

    [IMP] mail,hr: improve auto-join mechanism of discuss channels
    Goal of this commit is to improve the auto-join feature of discuss channels
    to auto-subscribe users based on their company, department, and groups.
    
    task-4804991

--- addons/hr/models/discuss_channel.py
+++ addons/hr/models/discuss_channel.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo import _, fields, models
+from odoo.fields import Domain
 
 
 class DiscussChannel(models.Model):
@@ -12,21 +12,19 @@ class DiscussChannel(models.Model):
         'hr.department', string='HR Departments',
         help='Automatically subscribe members of those departments to the channel.')
 
-    @api.constrains('subscription_department_ids')
-    def _constraint_subscription_department_ids_channel(self):
-        failing_channels = self.sudo().filtered(lambda channel: channel.channel_type != 'channel' and channel.subscription_department_ids)
-        if failing_channels:
-            raise ValidationError(_("For %(channels)s, channel_type should be 'channel' to have the department auto-subscription.", channels=', '.join([ch.name for ch in failing_channels])))
-
-    def _subscribe_users_automatically_get_members(self):
-        """ Auto-subscribe members of a department to a channel """
-        new_members = super()._subscribe_users_automatically_get_members()
-        for channel in self:
-            new_members[channel.id] = list(
-                set(new_members[channel.id]) |
-                set((channel.subscription_department_ids.member_ids.user_id.partner_id.filtered(lambda p: p.active) - channel.channel_partner_ids).ids)
+    def _get_auto_subscribe_domain(self):
+        domain = super()._get_auto_subscribe_domain()
+        # sudo: hr.department - ensure all departments and its members are accesible
+        sudo_departments = self.sudo().subscription_department_ids
+        if sudo_departments:
+            department_domain = Domain(
+                'id', 'in', sudo_departments.member_ids.user_id.ids,
             )
-        return new_members
+            if self.group_ids:
+                domain &= Domain('all_group_ids', 'in', self.group_ids.ids) | department_domain
+            else:
+                domain &= department_domain
+        return domain
 
     def write(self, vals):
         res = super().write(vals)
