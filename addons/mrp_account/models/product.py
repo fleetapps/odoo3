58f8669570f86b263a66b0a91786168cf3890230
agbr-odoo <agbr@odoo.com>
2026-01-15 12:20:01 +0530

    [FIX] mrp_account: avoid valuating kit products
    Currently, when a user creates a kit, the price of the kit itself is included in
    stock valuation.
    
    Steps to produce:
    * Install mrp_account without demo data.
    * Create a product with inventory tracking enabled.
    * Create a BoM of type kit for that product.
    * Add component products with a defined cost and on-hand quantity greater than 0
      to the BoM.
    * Recompute the kit product’s cost from its BoM on the product page.
    * Go to Inventory > Reporting > Stock.
    
    Observed Behavior:
    The cost of the kit is currently being included in the stock valuation.
    
    For example, consider a kit product called “Computer” that is composed of the
    following components:
    
    CPU, quantity 1, cost $300
    Motherboard, quantity 1, cost $300
    
    The total cost of the Computer kit is therefore $600. Since the Computer is made
    up of the CPU and Motherboard, the total inventory value should be $600.
    However, the system is currently calculating the total inventory value as $1,200
    , which is incorrect because it is counting both the kit and its components.
    
    Root cause:
    This behavior started after the refactor in [1], where the `_compute_value_svl`
    function was replaced by the `compute_value` function to calculate both average
    and total inventory valuation.
    
    With this change, the new compute function in [2] now also includes kit products
    when calculating inventory valuation based on their costing method.
    
    In earlier versions, this did not occur because `_compute_value_svl` depended on
    valuation layer groups. Kit products were excluded at [3] through the
     `_get_valuation_layer_groups()` call, as illustrated in image [4].
    
    Solution:
    To ensure correct total inventory valuation, kit products should be excluded
    from valuation and only their individual components should be calculated.
    This can be achieved by overriding the compute method so that kit products are
    filtered out during the valuation process, allowing the calculation to consider
    only their component items.
    
    [1]:
    odoo@6e694b7
    [2]:
    https://github.com/odoo/odoo/blob/1b9937a702fbeb47cd6d42d8119cead5828fd3fe/addons/stock_account/models/product.py#L139-L169
    [3]:
    https://github.com/odoo/odoo/blob/a9d2e54201173d1d2d5ab97de0904d63a4b6b82b/addons/stock_account/models/product.py#L284
    [4]:
    https://drive.google.com/file/d/1g4BzGscCW2K0rf5iDKrq-psYRlKhYkDP/view?usp=sharing
    
    opw-5462515

--- addons/mrp_account/models/product.py
+++ addons/mrp_account/models/product.py
@@ -98,6 +98,16 @@ class ProductProduct(models.Model):
             return bom.product_uom_id._compute_price(total / bom.product_qty, self.uom_id)
         return 0.0
 
+    def _compute_value(self):
+        """
+        Exclude kit products from inventory valuation to avoid double counting.
+        Only non-kit products are valuated; kits are set to zero value.
+        """
+        non_kit_products = self.filtered(lambda product: not product.is_kits)
+        super(ProductProduct, non_kit_products)._compute_value()
+        kit_products = self - non_kit_products
+        kit_products.update({"total_value": 0.0, "avg_cost": 0.0, "company_currency_id": self.env.company.currency_id})
+
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
