aad948f78791751a5b20d641429276a5cfe4c00c
Florian Malfroid <flmal@odoo.com>
2026-01-21 14:07:45 +0100

    [IMP] html_builder: Fix error when adding record with model_many2many
    **Issue**:
    When using the model_many2many component, if we create a new record then
    try to add another one, we get the following error:
    psycopg2.errors.InvalidTextRepresentation: invalid input syntax for type integer: "new-xxx"
    
    **Steps to reproduce**:
    1. Edit a blog post
    2. Select the title so that the list of tags appears in the editor
    3. Create a new tag.
    4. Try to add another tag
    
    **Fix:**
    Removed "new-" from the new ids to keep only an integer.
    
    Task-5215982

--- addons/html_builder/static/src/core/building_blocks/model_many2many.js
+++ addons/html_builder/static/src/core/building_blocks/model_many2many.js
@@ -96,7 +96,7 @@ export class ModelMany2Many extends Component {
         this.setSelection([
             ...this.domState.selection,
             {
-                id: `new-${uniqueId()}`,
+                id: `${uniqueId()}`,
                 name: name,
                 display_name: name,
                 model: this.state.searchModel,
