a4da28c551a6f74b6fdfbea0aa6dde77af7ee050
Jorge Pinna Puissant <jpp@odoo.com>
2026-01-28 10:51:54 +0100

    [IMP] web: enable creating and modifying records while offline
    This commit allows users to create and modify records while offline.
    
    To create a new record when offline, the user must first create a record when
    online. This sets the corresponding `on_change` for the creation of the record
    in the cache. The creation is limited to the kanban quick create, and to the
    form view.
    
    To edit an existing record, it needs to be in the cache. Therefore, the user
    must have visited the record while online previously. The edition of an
    existing record is limited to the editables lists, and the form view.
    
    After the user modifies or creates records, the modifications will appear in
    the "Working Offline" dropdown on the navbar. These records will be
    synchronized as soon as the user go back online.
    
    However, there are some limitations. Only the following field types can be
    created or modified when offline : boolean, JSON, integer, float, date,
    datetime, char, text, and HTML.
    
    task-id 5429930

--- addons/web/static/src/views/list/list_renderer.js
+++ addons/web/static/src/views/list/list_renderer.js
@@ -901,7 +901,12 @@ export class ListRenderer extends Component {
     isRecordAvailable(record) {
         return (
             !this.offlineService.offline ||
-            this.offlineService.isAvailableOffline(this.env.config.actionId, "form", record.resId)
+            this.offlineService.isAvailableOffline(
+                this.env.config.actionId,
+                "form",
+                record.resId
+            ) ||
+            this.isInlineEditable(record)
         );
     }
 
@@ -2325,13 +2330,5 @@ export class ListRenderer extends Component {
             ev.preventDefault();
             this.toggleRecordSelection(record);
         }
-    }
-
-    onOptionalColumnsDropdownOpened() {
-        this.optionalColumnsDropdownRef.el
-            .querySelectorAll(".o-dropdown-item input[type='checkbox']")
-            .forEach((checkbox) => {
-                checkbox.setAttribute("data-available-offline", "");
-            });
     }
 }
