f50eb80c0d53bd0fb7d7c86ec35c77c75e3ac792
pash-odoo <pash@odoo.com>
2026-01-30 12:46:59 +0530

    [IMP] data_recycle: allow user to manual refresh
    Before:
    - User was not able to refresh deduplication, recycle, and field cleaning data
      for all rules at once.
    - To refresh data, user had to open each rule and update records manually.
    - Records that no longer matched any rule were not automatically deleted.
    
    After:
    - Added a button in the cog menu to refresh deduplication, recycle, and field
      cleaning data for all rules at once.
    - Records that no longer match any rule are automatically deleted during
      the refresh process.
    
    task-5864554

---
+++ addons/data_recycle/static/src/cog_menu/data_refresh_cog_menu.js
@@ -0,0 +1,50 @@
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { registry } from "@web/core/registry";
+import { Component } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+
+const cogMenuRegistry = registry.category("cogMenu");
+
+export class DataRefreshCogMenu extends Component {
+    static template = "data_recycle.DataRefreshCogMenu";
+    static components = { DropdownItem };
+    static props = {};
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    async refreshData() {
+        const model = this.action.currentController?.props?.resModel;
+        const searchModel = this.env.searchModel;
+
+        if (model === "data_recycle.record") {
+            // The searchpanel adds the selected rule to the domain as
+            // ['recycle_model_id', '=', <id>]. We extract that id from the
+            // current domain and pass it in the action context so that,
+            // after refresh, the same rule remains selected in seachpanel.
+            const rule_id = searchModel.domain.filter(Array.isArray).find(
+                ([field, operator]) => field === "recycle_model_id" && operator === "="
+            )?.[2] ?? null;
+
+            await this.action.doActionButton({
+                type: "object",
+                resModel: "data_recycle.model",
+                name: "refresh_recycle_records",
+                context: {
+                    recycle_model_id: rule_id,
+                }
+            });
+        }
+    }
+}
+
+export const DataRefreshCogMenuItem = {
+    Component: DataRefreshCogMenu,
+    isDisplayed: ({ searchModel }) => {
+        return searchModel.resModel === "data_recycle.record";
+    },
+    groupNumber: 50,
+};
+
+cogMenuRegistry.add("data_refresh_menu", DataRefreshCogMenuItem, { sequence: 10 });
