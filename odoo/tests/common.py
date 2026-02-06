5998b4aabc52f3e194943030df411715d7f31887
thle-odoo <thle@odoo.com>
2026-02-06 15:33:59 +0100

    [IMP] tools: add `safe_eval` runtime analysis
    The execution of arbitrary code in Odoo via `safe_eval` already takes
    place in a restricted environment, in particular via prior opcode,
    `co_names` static analysis and specific builtins in the evaluation
    context.
    
    The purpose of this commit is to introduce the ability to dynamically
    analyse code executed via the `safe_eval` tool.
    
    The dangerous situation we want to avoid is transferring logic to code
    that is considered untrusted. In other words, we want to avoid calls
    that use objects that are not in the whitelist.
    
    The behavior to adopt when detecting an unsafe object depends on the
    `--unsafe-policy` configuration parameter:
    - `disable`: Disable the feature
    - `log`: Log a warning (default)
    - `raise`: Raise an exception
    - `terminate`: Terminate the process
    
    Note:
    Python 3.12+ is required since the [sys.monitoring](https://docs.python.org/3.12/library/sys.monitoring.html) API is used.
    The presence of the JSON C extension library is required (`_json`) for performance reasons.
    
    _This is not a sandbox in general, but it helps prevent dangerous
    behavior in the context of `safe_eval`._
    
    Task-4658036

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -74,6 +74,7 @@ from odoo.sql_db import Cursor
 from odoo.tools import SQL, DotDict, config, float_compare, mute_logger, profiler
 from odoo.tools.mail import single_email_re
 from odoo.tools.misc import diff_zip, find_in_path, lower_logging
+from odoo.tools.safe_eval import safe_whitelist
 from odoo.tools.xml_utils import _validate_xml
 
 import odoo.addons.base
@@ -2818,3 +2819,10 @@ class freeze_time:
 
 
 freezegun.freeze_time = freeze_time
+
+safe_whitelist.add_class('freezegun.api.FakeDate')
+safe_whitelist.add_class('freezegun.api.FakeDatetime')
+safe_whitelist.add_instance('odoo.sql_db.TestCursor')
+safe_whitelist.add_instance('odoo.tests.*')
+safe_whitelist.add_instance('unittest.mock.MagicMock')
+safe_whitelist.add_instance('unittest.mock.Mock')
