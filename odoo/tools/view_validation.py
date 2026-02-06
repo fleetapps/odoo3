787726bcd5e16d55665b8f41bcf827f9e0a1f30c
Victor Piryns (pivi) <pivi@odoo.com>
2026-02-06 17:00:14 +0100

    [IMP] tools: Support f-string for `get_expression_field_names`
    This commit allows inferring the missing field names when they are
    inside an f-string (single-level).
    
    task-4618828

--- odoo/tools/view_validation.py
+++ odoo/tools/view_validation.py
@@ -222,6 +222,13 @@ def _get_expression_contextual_values(item_ast):
         for item in item_ast.values:
             values |= _get_expression_contextual_values(item)
         return values
+    if isinstance(item_ast, ast.JoinedStr):
+        values = set()
+        for value in item_ast.values:
+            values |= _get_expression_contextual_values(value)
+        return values
+    if isinstance(item_ast, ast.FormattedValue):
+        return _get_expression_contextual_values(item_ast.value)
 
     raise ValueError(f"Undefined item {item_ast!r}.")
 
