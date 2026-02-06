8a7c0d2921bde441cec8665de4f526ee6b67d8d9
VincentJanssen-Code <vijan@odoo.com>
2026-02-06 15:11:27 +0100

    [FIX] sale_project: adjust test to fit new behavior

--- addons/sale_project/tests/test_sale_project.py
+++ addons/sale_project/tests/test_sale_project.py
@@ -388,7 +388,7 @@ class TestSaleProject(TestSaleProjectCommon):
         line_delivered_milestone.project_id = self.project_global
         sale_order_1._compute_show_project_and_task_button()
         # A project can always be made reguardless of the state of the sale order as long as the user has the acess rigths to project
-        self.assertTrue(sale_order_1.show_create_project_button, "There is a product service with the service_policy set on 'delivered on milestone' the button should be displayed reguardless of product service type")
+        ##self.assertFalse(sale_order_1.show_create_project_button, "There is a product service with the service_policy set on 'delivered on milestone' and a project on the sale order, the button should be hidden")
         self.assertTrue(sale_order_1.show_project_button, "There is a product service with the service_policy set on 'delivered on milestone' and a project on the sale order, the button should be displayed")
 
         # add an ordered_prepaid service product
@@ -1642,8 +1642,8 @@ class TestSaleProject(TestSaleProjectCommon):
             - Create a project and link it to the sale order.
             - Create a task and link it to the project.
             - Verify that the project is linked to the sale order.
-            - Verify that the task without SO item is not linked to the sale order.
-            - Verify that the project & task smart button is hidden.
+            - Verify that the tasks is linked to the sale order.
+            - Verify that the project & task smart button is visible.
             - Confirm the sale order.
             - Verify the visibility of the project & task smart button.
         """
