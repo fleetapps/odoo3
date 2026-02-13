4b8bd79c23e28e2df44e118ef8a770bd5644f2ee
Léo Gizard <legi@odoo.com>
2026-02-13 10:17:59 +0100

    [FIX] l10n_latam_base: changes identification type on country change
    **PROBLEM**
    PR: https://github.com/odoo/odoo/pull/179078
    Removed _onchange_country_id() which was used to set the identification type
    according to the country of the partner.
    This PR reintroduce it, so id type and country remains consistent.
    
    **STEP TO REPRODUCE**
    1. install l10n_ar and l10n_co.
    2. create a new partner.
    3. set its country to Argentina, and select an argentinian id type.
    4. set the country to Colombia and save.
    You end up with a partner from Colombia, with a id type that is used
    for Argentinian partners which shouldn't be possible.
    
    opw-5801824

--- addons/l10n_latam_base/models/res_partner.py
+++ addons/l10n_latam_base/models/res_partner.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
@@ -18,7 +19,9 @@ class ResPartner(models.Model):
         return super()._commercial_fields() + ['l10n_latam_identification_type_id']
 
     def _run_check_identification(self, validation='error'):
-        return
+        for partner in self:
+            if partner.country_id and partner.l10n_latam_identification_type_id.country_id and partner.l10n_latam_identification_type_id.country_id != partner.country_id:
+                raise ValidationError(_('The indentification type must match the partner country.'))
 
     def _check_vat(self, validation='error'):
         partners_vat = self.filtered(lambda p:
@@ -27,6 +30,17 @@ class ResPartner(models.Model):
         )
         super(ResPartner, partners_vat)._check_vat(validation=validation)
         (self - partners_vat)._run_check_identification(validation=validation)
+
+    @api.onchange('country_id')
+    def _onchange_country_id(self):
+        country = self.country_id or self.company_id.account_fiscal_country_id or self.env.company.account_fiscal_country_id
+        identification_type = self.l10n_latam_identification_type_id
+        if not identification_type or (identification_type.country_id != country):
+            self.l10n_latam_identification_type_id = (
+                self.env['l10n_latam.identification.type'].search(
+                [('country_id', '=', country.id), ('is_vat', '=', True)], limit=1) or
+                self.env.ref('l10n_latam_base.it_vat', raise_if_not_found=False)
+            )
 
     @api.onchange('vat', 'country_id', 'l10n_latam_identification_type_id')
     def _onchange_vat(self):
