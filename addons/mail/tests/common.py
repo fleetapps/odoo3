cc58dbaa40d0702f3079a73367ada6fd9d35981d
Thibault Delavallée <tde@odoo.com>
2026-02-13 08:55:52 +0100

    [IMP] mail, various: update tracking test tooling
    When testing tracking values we often have side values to test:
    currency of monetary fields, information on property fields, ...
    Let us update a bit the tooling used to check tracking values.
    It currently relies on splitting tuples, but it is simpler to
    accept additional arguments.
    
    Task-5935695 ([mail, various] Cleanup and test tracking usage)
    Prepares Task-3645865 ([mail] In-body tracking)
    
    Co-Authored-By: Prakash Prajapati <ppr@odoo.com>

--- addons/mail/tests/common.py
+++ addons/mail/tests/common.py
@@ -810,6 +810,9 @@ class MockEmail(common.BaseCase, MockSmtplibCase):
                 # tracking values themselves, a shortcut
                 elif fname == 'tracking_values':
                     self.assertTracking(message, fvalue, strict=True)
+                # body content: not strict equal, just check given content is inside it
+                elif fname == 'body_content':
+                    self.assertIn(fvalue, message['body'])
                 else:
                     self.assertEqual(
                         message[fname], fvalue,
@@ -1041,23 +1044,25 @@ class MockEmail(common.BaseCase, MockSmtplibCase):
             for t in tracking_values
         )
         if strict:
-            exp_fnames = sorted([i[0][0] if isinstance(i[0], tuple) else i[0] for i in data])
+            exp_fnames = sorted([i[0] or '' for i in data])
             fnames = sorted([t.field_id.name or '' for t in tracking_values])
             info = f'Field names: expected {exp_fnames}, received {fnames}'
             self.assertEqual(len(tracking_values), len(data),
                              f'Tracking: invalid number of tracking: {info}\n{tracking_info}')
 
-        for field_name, value_type, old_value, new_value in data:
-            # allow giving field_info in addition to field_name (a bit hackish but easier
-            # than updating all calls)
-            if isinstance(field_name, tuple):
-                field_name, field_info = field_name
+        for tracking_values_info in data:
+            if len(tracking_values_info) == 5:
+                field_name, value_type, old_value, new_value, additional_info = tracking_values_info
             else:
-                field_info = {}
+                field_name, value_type, old_value, new_value = tracking_values_info
+                additional_info = {}
+            # retrieve optional field info, used notably for dummy tracking or properties
+            field_info = additional_info.get('field_info')
 
             # for property fields, value_type is a tuple for the embed property value
-            if isinstance(value_type, tuple):
-                value_type, prop_field_string, prop_type = value_type
+            if value_type == 'properties':
+                prop_field_string = additional_info['prop_field_string']
+                prop_type = additional_info['prop_type']
                 tracking = tracking_values.filtered(lambda track: track.field_id.name == field_name and prop_field_string == (track.field_info or {}).get('desc'))
                 self.assertEqual(
                     len(tracking), 1,
@@ -1081,9 +1086,10 @@ class MockEmail(common.BaseCase, MockSmtplibCase):
             self.assertTrackingValue(
                 tracking, prop_field_string or field_name, prop_type or value_type,
                 old_value, new_value,
+                additional_info=additional_info,
             )
 
-    def assertTrackingValue(self, tracking, field_name, value_type, old_value, new_value):
+    def assertTrackingValue(self, tracking, field_name, value_type, old_value, new_value, additional_info=None):
         suffix_mapping = {
             'boolean': 'integer',
             'char': 'char',
@@ -1111,12 +1117,16 @@ class MockEmail(common.BaseCase, MockSmtplibCase):
             self.assertEqual(tracking.old_value_char, (old_value and old_value.display_name) or '')
             self.assertEqual(tracking.new_value_char, (new_value and new_value.display_name) or '')
         elif value_type == 'monetary':
-            new_value, currency = new_value
+            currency = (additional_info or {})['currency']
             self.assertEqual(tracking.currency_id, currency)
             self.assertEqual(tracking.old_value_float, old_value)
             self.assertEqual(tracking.new_value_float, new_value)
         else:
             self.assertEqual(1, 0, f'Tracking: unsupported tracking test on {value_type}')
+
+        if (additional_info or {}).get('field_info'):
+            for key, val in additional_info['field_info'].items():
+                self.assertEqual(tracking.field_info[key], val)
 
 
 class MailCase(common.TransactionCase, MockEmail, BusCase):
