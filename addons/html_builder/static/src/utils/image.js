0b1f42c3852791aef7c3400cd59df92c64e658f3
Louis (loco) <loco@odoo.com>
2026-02-13 15:13:40 +0100

    [FIX] html_builder, html_editor: enable the quality change on shaped img
    Steps to reproduce:
    - Add an 'Image-Text` snippet on the website and replace the image by a
    `jpeg` (note that you can modify the quality of the image).
    - Add a shape on the image.
    
    -> The "Quality" option disappeared.
    
    The problem is that the option checked at the current mimetype of the
    image to determine if the option should appear or not (the option should
    not appear for native `svg` or `png` images as the option does not have
    any impact on them due to the nature of those formats). To solve the
    problem, the mimetype of the image before a shape has been applied is
    evaluated to show the "quality" option.
    
    task-5405262

---
+++ addons/html_builder/static/src/utils/image.js
@@ -0,0 +1,11 @@
+import { loadImageInfo } from "@html_editor/utils/image_processing";
+import { getMimetype } from "@html_editor/utils/image";
+
+export async function getMimetypeBeforeShape(imageEl, data = imageEl.dataset) {
+    const { formatMimetype, mimetypeBeforeConversion } = data.mimetypeBeforeConversion
+        ? data
+        : await loadImageInfo(imageEl);
+    return formatMimetype || mimetypeBeforeConversion
+        ? formatMimetype || mimetypeBeforeConversion
+        : getMimetype(imageEl, data);
+}
