89260fdf9f8911171b3bb86770ec6b15c0e6e31b
Benjamin Vray <bvr@odoo.com>
2026-02-16 17:11:10 +0100

    [IMP] html_builder, website, *: improve dropzone and overlay visibility
    *: website_blog
    
    Steps to reproduce (inner dropzones):
    
    - Open any website page in edit mode.
    - Drag an "inner snippet" over another snippet.
    - Check the inner dropzones while dragging.
    => Inner dropzones are as thick as block dropzones.
    
    Before this commit, inner dropzones had the same thickness as block
    dropzones, which made inner editing areas look too heavy.
    
    After this commit, inner dropzones are thinner than block dropzones.
    
    Steps to reproduce (blue background contrast):
    
    - Open any website page in edit mode.
    - Set a snippet background close to the dropzone blue, for example
      color: #00B8C2.
    - Drag a block around this snippet.
    => Dropzones and overlay edges are hard to see.
    
    Before this commit, dropzones and overlay edges could lose contrast on
    blue backgrounds close to #00B8C2.
    
    After this commit, dropzones and overlay edges keep clear contrast on
    blue backgrounds, including colors close to #00B8C2.
    
    This commit also improves sanitized dropzone rendering and message
    clarity.
    
    task-5921283

--- addons/html_builder/static/src/core/drop_zone_plugin.js
+++ addons/html_builder/static/src/core/drop_zone_plugin.js
@@ -364,9 +364,9 @@ export class DropZonePlugin extends Plugin {
             "oe_sanitized_drop_zone",
             "text-center"
         );
-        const messageEl = this.document.createElement("p");
-        messageEl.textContent = _t("For technical reasons, this block cannot be dropped here");
-        dropzoneEl.prepend(messageEl);
+        dropzoneEl.dataset.editorMessage = _t(
+            "For technical reasons, this block cannot be dropped here"
+        );
         return dropzoneEl;
     }
 
@@ -516,7 +516,6 @@ export class DropZonePlugin extends Plugin {
 
         // Inserting a sanitized dropzone for each sanitized area.
         for (const sanitizedZoneEl of selectorSanitized) {
-            sanitizedZoneEl.style.position = "relative";
             sanitizedZoneEl.prepend(this.createSanitizedDropzone());
         }
         this.sanitizedZoneEls = selectorSanitized;
