c842a382f7975727b5b6b11fc4e10d789a7a6b6e
Bertrand Dossogne <bedo@odoo.com>
2026-02-18 12:24:46 +0000

    [FIX] hr_holidays: fix calendar view tour
    This fix adjusts the tour in `test_hours_time_off_request_calendar_view`
    as it was failing in some cases.
    
    runbot error 237682
    
    X-original-commit: bfc1c210eca65de1116aa09cdd62749ed9192407

--- addons/hr_holidays/static/tests/tours/time_off_request_calendar_view.js
+++ addons/hr_holidays/static/tests/tours/time_off_request_calendar_view.js
@@ -11,6 +11,7 @@ registry.category("web_tour.tours").add("time_off_request_calendar_view", {
         },
         {
             content: "Click on the first Thursday of the year",
+<<<<<<< f5f29240f10f76dce15e34e2617ae4542cb0564c
             trigger: ".fc-daygrid-day.fc-day-thu",
             run: () => {
                 const el = document.querySelector(
@@ -41,10 +42,43 @@ registry.category("web_tour.tours").add("time_off_request_calendar_view", {
                     })
                 );
             },
+||||||| d2799f147923e2a028aeb2a12c409e104cc7eddc
+            trigger: ".fc-daygrid-day.fc-day-thu",
+            run: () => {
+                const el = document.querySelector(".fc-daygrid-day.fc-day-thu:not(.fc-day-disabled)").firstChild;
+                el.scrollIntoView();
+
+                const fromPosition = el.getBoundingClientRect();
+                fromPosition.x += el.offsetWidth / 2;
+                fromPosition.y += el.offsetHeight / 2;
+
+                el.dispatchEvent(
+                    new MouseEvent("mousedown", {
+                        bubbles: true,
+                        which: 1,
+                        button: 0,
+                        clientX: fromPosition.x,
+                        clientY: fromPosition.y,
+                    })
+                );
+                el.dispatchEvent(
+                    new MouseEvent("mouseup", {
+                        bubbles: true,
+                        which: 1,
+                        button: 0,
+                        clientX: fromPosition.x,
+                        clientY: fromPosition.y,
+                    })
+                );
+            },
+=======
+            trigger: ".fc-daygrid-day.fc-day-thu:not(.fc-day-disabled)",
+            run: "click",
+>>>>>>> f18fd67c25aa22c82b96d6fe069a41bcac8bc50a
         },
         {
             content: "Save the leave",
-            trigger: '.btn:contains("Submit Request")',
+            trigger: '.o_form_button_save',
             run: "click",
         },
     ],
