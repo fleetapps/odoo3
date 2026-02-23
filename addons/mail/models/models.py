odoo/odoo

[MOV] mail,*: move recipient info logic to mail
*=sms,mass_mailing_sms

Moved `_sms_get_recipients_info` from the SMS module to the Mail module
as `_mail_get_recipients_info`. This centralizes the logic for retrieving
recipient phone numbers, so it can also be used when sending WhatsApp
templates with the default recipient feature.

Task-4213035

addons/mail/models/models.py
@@ -910,3 +910,106 @@ class Base(models.AbstractModel):
             None,
             (self[tz_field] for tz_field in ('date_tz', 'tz', 'timezone') if tz_field in self)
         ), None)
+
+    def _mail_get_recipients_info(self, force_field=False, partner_fallback=True):
+        """ Get recipient information on current record set. This method
+        checks for numbers and sanitation in order to centralize computation.
+
+        Example of use cases
+
+          * click on a field -> number is actually forced from field, find customer
+            linked to record, force its number to field or fallback on customer fields;
+          * contact -> find numbers from all possible phone fields on record, find
+            customer, force its number to found field number or fallback on customer fields;
+
+        :param force_field: either give a specific field to find phone number, either
+            generic heuristic is used to find one based on :meth:`_phone_get_number_fields`;
+        :param partner_fallback: if no value found in the record, check its customer
+            values based on :meth:`_mail_get_partners`;
+
+        :rtype: dict[int, dict[str, Any]]
+        :return: a dictionnary with the following structure:
+
+            .. code-block:: python
+
+                {
+                    record.id: {
+                        # a res.partner recordset that is the customer (void or
+                        # singleton) linked to the recipient.
+                        # See _mail_get_partners;
+                        'partner': ...,
+
+                        # sanitized number to use (coming from record's field
+                        # or partner's phone fields). Set to False if number
+                        # impossible to parse and format;
+                        'sanitized': ...,
+
+                        # original number before sanitation;
+                        'number': ...,
+
+                        # whether the number comes from the customer phone
+                        # fields. If False it means number comes from the
+                        # record itself, even if linked to a customer;
+                        'partner_store': ...,
+
+                        # field in which the number has been found (generally
+                        # mobile or phone, see _phone_get_number_fields);
+                        'field_store': ...,
+                    }
+                    for record in self
+                }
+
+        """
+        result = dict.fromkeys(self.ids, False)
+        tocheck_fields = [force_field] if force_field else self._phone_get_number_fields()
+        for record in self:
+            all_numbers = [record[fname] for fname in tocheck_fields if fname in record]
+            all_partners = record._mail_get_partners()[record.id]
+
+            valid_number, fname = False, False
+            for fname in [f for f in tocheck_fields if f in record]:
+                valid_number = record._phone_format(fname=fname)
+                if valid_number:
+                    break
+
+            if valid_number:
+                result[record.id] = {
+                    'partner': all_partners[0] if all_partners else self.env['res.partner'],
+                    'sanitized': valid_number,
+                    'number': record[fname],
+                    'partner_store': False,
+                    'field_store': fname,
+                }
+            elif all_partners and partner_fallback:
+                partner = self.env['res.partner']
+                for partner in all_partners:
+                    for fname in self.env['res.partner']._phone_get_number_fields():
+                        valid_number = partner._phone_format(fname=fname)
+                        if valid_number:
+                            break
+
+                if not valid_number:
+                    fname = 'phone'
+
+                result[record.id] = {
+                    'partner': partner,
+                    'sanitized': valid_number if valid_number else False,
+                    'number': partner[fname],
+                    'partner_store': True,
+                    'field_store': fname,
+                }
+            else:
+                # did not find any sanitized number -> take first set value as fallback;
+                # if none, just assign False to the first available number field
+                value, fname = next(
+                    ((value, fname) for value, fname in zip(all_numbers, tocheck_fields) if value),
+                    (False, tocheck_fields[0] if tocheck_fields else False)
+                )
+                result[record.id] = {
+                    'partner': self.env['res.partner'],
+                    'sanitized': False,
+                    'number': value,
+                    'partner_store': False,
+                    'field_store': fname
+                }
+        return result

addons/sms/models/mail_thread.py
@@ -111,7 +111,7 @@ class MailThread(models.AbstractModel):
         sms_pid_to_number = sms_pid_to_number if sms_pid_to_number is not None else {}
 
         if number_field or (partner_ids is False and sms_numbers is None):
-            info = self._sms_get_recipients_info(force_field=number_field)[self.id]
+            info = self._mail_get_recipients_info(force_field=number_field)[self.id]
             info_partner_ids = info['partner'].ids if info['partner'] else False
             info_number = info['sanitized'] if info['sanitized'] else info['number']
             if info_partner_ids and info_number:

addons/sms/wizard/sms_composer.py
@@ -106,7 +106,7 @@ class SmsComposer(models.TransientModel):
 
             records = composer._get_records()
             if records:
-                res = records._sms_get_recipients_info(force_field=composer.number_field_name, partner_fallback=not composer.comment_single_recipient)
+                res = records._mail_get_recipients_info(force_field=composer.number_field_name, partner_fallback=not composer.comment_single_recipient)
                 composer.recipient_valid_count = len([rid for rid, rvalues in res.items() if rvalues['sanitized']])
                 composer.recipient_invalid_count = len([rid for rid, rvalues in res.items() if not rvalues['sanitized']])
             else:
@@ -123,7 +123,7 @@ class SmsComposer(models.TransientModel):
                 continue
             records.ensure_one()
             # If the composer was opened with a specific field use that, otherwise get the partner's
-            res = records._sms_get_recipients_info(force_field=composer.number_field_name, partner_fallback=not composer.number_field_name)
+            res = records._mail_get_recipients_info(force_field=composer.number_field_name, partner_fallback=not composer.number_field_name)
             if not composer.recipient_single_number_itf:
                 composer.recipient_single_number_itf = res[records.id]['sanitized'] or res[records.id]['number'] or ''
             if not composer.number_field_name:
@@ -138,7 +138,7 @@ class SmsComposer(models.TransientModel):
                 composer.recipient_single_number = ''
                 continue
             records.ensure_one()
-            res = records._sms_get_recipients_info(force_field=composer.number_field_name, partner_fallback=True)
+            res = records._mail_get_recipients_info(force_field=composer.number_field_name, partner_fallback=True)
             composer.recipient_single_description = res[records.id]['partner'].name or records._mail_get_partners()[records[0].id].display_name
             composer.recipient_single_number = res[records.id]['sanitized'] or res[records.id]['number'] or ''
 
@@ -302,7 +302,7 @@ class SmsComposer(models.TransientModel):
         return done_ids
 
     def _prepare_recipient_values(self, records):
-        recipients_info = records._sms_get_recipients_info(force_field=self.number_field_name)
+        recipients_info = records._mail_get_recipients_info(force_field=self.number_field_name)
         return recipients_info
 
     def _prepare_body_values(self, records):

fleetapps/odoo3

addons/mail/models/models.py
@@ -943,3 +943,107 @@ class Base(models.AbstractModel):
             None,
             (self[tz_field] for tz_field in ('date_tz', 'tz', 'timezone') if tz_field in self)
         ), None)
+
+    def _mail_get_recipients_phone_info(self, force_field=False, partner_fallback=True):
+        """ Get recipient phone related information on current record set. This method
+        checks for numbers and sanitation in order to centralize computation.
+
+        Example of use cases
+
+          * click on a field -> number is actually forced from field, find customer
+            linked to record, force its number to field or fallback on customer fields;
+          * contact -> find numbers from all possible phone fields on record, find
+            customer, force its number to found field number or fallback on customer fields;
+
+        :param force_field: either give a specific field to find phone number, either
+            generic heuristic is used to find one based on :meth:`_phone_get_number_fields`;
+        :param partner_fallback: if no value found in the record, check its customer
+            values based on :meth:`_mail_get_partners`;
+
+        :rtype: dict[int, dict[str, Any]]
+        :return: a dictionnary with the following structure:
+
+            .. code-block:: python
+
+                {
+                    record.id: {
+                        # a res.partner recordset that is the customer (void or
+                        # singleton) linked to the recipient.
+                        # See _mail_get_partners;
+                        'partner': ...,
+
+                        # sanitized number to use (coming from record's field
+                        # or partner's phone fields). Set to False if number
+                        # impossible to parse and format;
+                        'sanitized': ...,
+
+                        # original number before sanitation;
+                        'number': ...,
+
+                        # whether the number comes from the customer phone
+                        # fields. If False it means number comes from the
+                        # record itself, even if linked to a customer;
+                        'partner_store': ...,
+
+                        # field in which the number has been found (generally
+                        # mobile or phone, see _phone_get_number_fields);
+                        'field_store': ...,
+                    }
+                    for record in self
+                }
+
+        """
+        result = dict.fromkeys(self.ids, False)
+        tocheck_fields = [force_field] if force_field else self._phone_get_number_fields()
+        all_partners_by_record = self._mail_get_partners()
+        for record in self:
+            all_numbers = [record[fname] for fname in tocheck_fields if fname in record]
+            all_partners = all_partners_by_record[record.id]
+
+            valid_number, fname = False, False
+            for fname in [f for f in tocheck_fields if f in record]:
+                valid_number = record._phone_format(fname=fname)
+                if valid_number:
+                    break
+
+            if valid_number:
+                result[record.id] = {
+                    'partner': all_partners[0] if all_partners else self.env['res.partner'],
+                    'sanitized': valid_number,
+                    'number': record[fname],
+                    'partner_store': False,
+                    'field_store': fname,
+                }
+            elif all_partners and partner_fallback:
+                partner = self.env['res.partner']
+                for partner in all_partners:
+                    for fname in self.env['res.partner']._phone_get_number_fields():
+                        valid_number = partner._phone_format(fname=fname)
+                        if valid_number:
+                            break
+
+                if not valid_number:
+                    fname = 'phone'
+
+                result[record.id] = {
+                    'partner': partner,
+                    'sanitized': valid_number if valid_number else False,
+                    'number': partner[fname],
+                    'partner_store': True,
+                    'field_store': fname,
+                }
+            else:
+                # did not find any sanitized number -> take first set value as fallback;
+                # if none, just assign False to the first available number field
+                value, fname = next(
+                    ((value, fname) for value, fname in zip(all_numbers, tocheck_fields) if value),
+                    (False, tocheck_fields[0] if tocheck_fields else False)
+                )
+                result[record.id] = {
+                    'partner': self.env['res.partner'],
+                    'sanitized': False,
+                    'number': value,
+                    'partner_store': False,
+                    'field_store': fname
+                }
+        return result

addons/sms/models/mail_thread.py
@@ -111,7 +111,7 @@ class MailThread(models.AbstractModel):
         sms_pid_to_number = sms_pid_to_number if sms_pid_to_number is not None else {}
 
         if number_field or (partner_ids is False and sms_numbers is None):
-            info = self._sms_get_recipients_info(force_field=number_field)[self.id]
+            info = self._mail_get_recipients_phone_info(force_field=number_field)[self.id]
             info_partner_ids = info['partner'].ids if info['partner'] else False
             info_number = info['sanitized'] if info['sanitized'] else info['number']
             if info_partner_ids and info_number:

addons/sms/wizard/sms_composer.py
@@ -110,7 +110,7 @@ class SmsComposer(models.TransientModel):
 
             records = composer._get_records()
             if records:
-                res = records._sms_get_recipients_info(force_field=composer.number_field_name, partner_fallback=not composer.comment_single_recipient)
+                res = records._mail_get_recipients_phone_info(force_field=composer.number_field_name, partner_fallback=not composer.comment_single_recipient)
                 composer.recipient_valid_count = len([rid for rid, rvalues in res.items() if rvalues['sanitized']])
                 composer.recipient_invalid_count = len([rid for rid, rvalues in res.items() if not rvalues['sanitized']])
             else:
@@ -127,7 +127,7 @@ class SmsComposer(models.TransientModel):
                 continue
             records.ensure_one()
             # If the composer was opened with a specific field use that, otherwise get the partner's
-            res = records._sms_get_recipients_info(force_field=composer.number_field_name, partner_fallback=not composer.number_field_name)
+            res = records._mail_get_recipients_phone_info(force_field=composer.number_field_name, partner_fallback=not composer.number_field_name)
             if not composer.recipient_single_number_itf:
                 composer.recipient_single_number_itf = res[records.id]['sanitized'] or res[records.id]['number'] or ''
             if not composer.number_field_name:
@@ -142,7 +142,7 @@ class SmsComposer(models.TransientModel):
                 composer.recipient_single_number = ''
                 continue
             records.ensure_one()
-            res = records._sms_get_recipients_info(force_field=composer.number_field_name, partner_fallback=True)
+            res = records._mail_get_recipients_phone_info(force_field=composer.number_field_name, partner_fallback=True)
             composer.recipient_single_description = res[records.id]['partner'].name or records._mail_get_partners()[records[0].id].display_name
             composer.recipient_single_number = res[records.id]['sanitized'] or res[records.id]['number'] or ''
 
@@ -309,7 +309,7 @@ class SmsComposer(models.TransientModel):
         return done_ids
 
     def _prepare_recipient_values(self, records):
-        recipients_info = records._sms_get_recipients_info(force_field=self.number_field_name)
+        recipients_info = records._mail_get_recipients_phone_info(force_field=self.number_field_name)
         return recipients_info
 
     def _prepare_body_values(self, records):