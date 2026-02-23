yourjelly/odoo

[IMP] resource, test_resource: Prevent incoherent multi-company configurations for material resources
Prior to this commit, it was possible to select a working calendar tied
to company A, then to change the company of the resource to company B
while keeping company A's calendar, thus creating an incoherent situation.

After this commit, we set the the working calendar to False after
changing the company of a material resource to avoid these types of incoherence.
The reason why we don't set the working calendar to the new company's
default calendar are the following:
1. It was mentioned in this previous [PR](https://github.com/odoo/enterprise/pull/54692)
that setting a default calendar for a material is not always relevant, since they often
do not share the same constraints as human resources.
2. If we were to set the working calendar to the new company's default calendar, user
might not notice the change if the two calendars share the same name.
3. If the company of a calendar is modified while a resource is linked to it,
we return a validation error to prevent the user from creating an incoherent situation.

task: 3857551

addons/resource/models/resource_resource.py
@@ -44,6 +44,7 @@ class ResourceResource(models.Model):
     calendar_id = fields.Many2one(
         "resource.calendar", string='Working Time',
         default=lambda self: self.env.company.resource_calendar_id,
+        compute='_compute_calendar_id', store=True, readonly=False,
         domain="[('company_id', '=', company_id)]",
         help="Define the working schedule of the resource. If not set, the resource will have fully flexible working hours.")
     tz = fields.Selection(
@@ -88,10 +89,14 @@ class ResourceResource(models.Model):
             return True
         return super().write(values)
 
-    @api.onchange('company_id')
-    def _onchange_company_id(self):
-        if self.company_id:
-            self.calendar_id = self.company_id.resource_calendar_id.id
+    @api.depends('company_id')
+    def _compute_calendar_id(self):
+        for r in self:
+            if r.company_id and r.company_id != r.calendar_id.company_id:
+                if r.resource_type == 'material':
+                    r.calendar_id = False
+                else:
+                    r.calendar_id = r.company_id.resource_calendar_id
 
     @api.onchange('user_id')
     def _onchange_user_id(self):

fleetapps/odoo3

addons/resource/models/resource_resource.py
@@ -49,6 +49,7 @@ class ResourceResource(models.Model):
     calendar_id = fields.Many2one(
         "resource.calendar", string='Working Time',
         default=lambda self: self.env.company.resource_calendar_id,
+        compute='_compute_calendar_id', store=True, readonly=False,
         domain="[('company_id', '=', company_id)]",
         help="Define the working schedule of the resource. If not set, the resource will have fully flexible working hours.")
     hours_per_week = fields.Float(string="Hours per Week", compute='_compute_hours_per_week', store=True, readonly=False)
@@ -116,10 +117,14 @@ class ResourceResource(models.Model):
             if not resource.hours_per_week:
                 resource.hours_per_week = resource.hours_per_day * 7
 
-    @api.onchange('company_id')
-    def _onchange_company_id(self):
-        if self.company_id:
-            self.calendar_id = self.company_id.resource_calendar_id.id
+    @api.depends('company_id')
+    def _compute_calendar_id(self):
+        for resource in self:
+            if resource.company_id and resource.company_id != resource.calendar_id.company_id:
+                if resource.resource_type == 'material':
+                    resource.calendar_id = False
+                else:
+                    resource.calendar_id = resource.company_id.resource_calendar_id
 
     @api.onchange('user_id')
     def _onchange_user_id(self):