odoo/odoo

[IMP] account: update Accounting Firms mode settings
Previously, in Accounting Firms mode, users had to select the invoice types
on which all settings(document sequence editable, total amount
(tax-included), and date suggestion) would apply.

With this commit, this behavior is now split into two parts. Quick Encoding
controls the total amount and date suggestion features and applies only to the
selected invoice types. Document Sequence Editable is no longer invoice-type
specific and, when enabled, applies to all invoice types.

Additionally, journal entries created by standard accounting users (non-
bookkeepers) were previously always marked as To Review. They are now
marked as To Review only when the Review Data setting is enabled in
Accounting Firms mode.

task-5418617

addons/account/models/company.py
@@ -251,12 +251,16 @@ class ResCompany(models.Model):
     )
 
     # Fiduciary mode
+    quick_edit_mode_enabled = fields.Boolean(string="Quick encoding")
     quick_edit_mode = fields.Selection(
         selection=[
             ('out_invoices', 'Customer Invoices'),
             ('in_invoices', 'Vendor Bills'),
             ('out_and_in_invoices', 'Customer Invoices and Vendor Bills')],
-        string="Quick encoding")
+        default='out_and_in_invoices',
+    )
+    document_sequence_editable = fields.Boolean(string="Docuemnt's sequence editable")
+    set_to_review_documents = fields.Boolean(string="Review data")
 
     # Separate account for allocation of discounts
     account_discount_income_allocation_id = fields.Many2one(comodel_name='account.account', string='Separate account for income discount')

addons/account/models/res_config_settings.py
@@ -156,7 +156,10 @@ class ResConfigSettings(models.TransientModel):
     group_sale_delivery_address = fields.Boolean("Customer Addresses", implied_group='account.group_delivery_invoice_address')
 
     # Quick encoding (fiduciary mode)
-    quick_edit_mode = fields.Selection(string="Quick encoding", readonly=False, related='company_id.quick_edit_mode')
+    quick_edit_mode_enabled = fields.Boolean(string="Quick encoding", readonly=False, related='company_id.quick_edit_mode_enabled')
+    quick_edit_mode = fields.Selection(readonly=False, related='company_id.quick_edit_mode')
+    document_sequence_editable = fields.Boolean(string="Docuemnt's sequence editable", readonly=False, related='company_id.document_sequence_editable')
+    set_to_review_documents = fields.Boolean(string="Review data", readonly=False, related='company_id.set_to_review_documents')
 
     account_journal_early_pay_discount_loss_account_id = fields.Many2one(
         comodel_name='account.account',

fleetapps/odoo3

addons/account/models/company.py
@@ -251,12 +251,16 @@ class ResCompany(models.Model):
     )
 
     # Fiduciary mode
+    quick_edit_mode_enabled = fields.Boolean(string="Quick encoding")
     quick_edit_mode = fields.Selection(
         selection=[
             ('out_invoices', 'Customer Invoices'),
             ('in_invoices', 'Vendor Bills'),
             ('out_and_in_invoices', 'Customer Invoices and Vendor Bills')],
-        string="Quick encoding")
+        default='out_and_in_invoices',
+    )
+    document_sequence_editable = fields.Boolean(string="Document's sequence editable")
+    set_to_review_documents = fields.Boolean(string="Review data")
 
     # Separate account for allocation of discounts
     account_discount_income_allocation_id = fields.Many2one(comodel_name='account.account', string='Separate account for income discount')

addons/account/models/res_config_settings.py
@@ -156,7 +156,10 @@ class ResConfigSettings(models.TransientModel):
     group_sale_delivery_address = fields.Boolean("Customer Addresses", implied_group='account.group_delivery_invoice_address')
 
     # Quick encoding (fiduciary mode)
-    quick_edit_mode = fields.Selection(string="Quick encoding", readonly=False, related='company_id.quick_edit_mode')
+    quick_edit_mode_enabled = fields.Boolean(string="Quick encoding", readonly=False, related='company_id.quick_edit_mode_enabled')
+    quick_edit_mode = fields.Selection(readonly=False, related='company_id.quick_edit_mode')
+    document_sequence_editable = fields.Boolean(string="Document's sequence editable", readonly=False, related='company_id.document_sequence_editable')
+    set_to_review_documents = fields.Boolean(string="Review data", readonly=False, related='company_id.set_to_review_documents')
 
     account_journal_early_pay_discount_loss_account_id = fields.Many2one(
         comodel_name='account.account',