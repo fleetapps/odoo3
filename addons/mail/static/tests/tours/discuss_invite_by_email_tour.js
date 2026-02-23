dzdzdzdz1010/OdooStandard

[IMP] mail, livechat: forward option for messages
Adds message forwarding feature to Discuss and Livechat,
allowing users to share messages across channels and private chats efficiently.

Features:

- Forward messages to up to 5 destinations in one action
- Automatically duplicate and attach original message's attachments
- Include optional custom note with forwarded content
- Generate link previews when messages contain links
- Search destinations with recent suggestions

Dialog collects destinations and optional note, then calls /mail/message/forward
The controller validates post access per destination and calls
`discuss.channel._forward_message()`, which duplicates attachments
via `copy_data()`, creates new attachments per channel, and posts the original
message body with `forwarded_from_id` to track the source. The optional note is
posted as a separate message. Link previews are generated asynchronously when
applicable. The ForwardedMessage component displays forwarded messages with
a reference to the original conversation.

task-4633375

addons/mail/static/tests/tours/discuss_invite_by_email_tour.js
@@ -11,13 +11,13 @@ registry.category("web_tour.tours").add("discuss.invite_by_email", {
             run: "click",
         },
         {
-            trigger: ".o-discuss-ChannelInvitation-search[placeholder='Invite people or email']",
+            trigger: ".o-discuss-SelectableList-search[placeholder='Invite people or email']",
             run: "edit john@test.com",
         },
         {
-            trigger: ".o-discuss-ChannelInvitation-selectable:contains('john (base.group_user)')",
+            trigger: ".o-discuss-SelectableList-selectable:contains('john (base.group_user)')",
             async run({ waitFor, click }) {
-                await waitFor(".o-discuss-ChannelInvitation-selectable", {
+                await waitFor(".o-discuss-SelectableList-selectable", {
                     only: true,
                     timeout: 5000,
                 });
@@ -25,20 +25,18 @@ registry.category("web_tour.tours").add("discuss.invite_by_email", {
             },
         },
         {
-            trigger:
-                ".o-discuss-ChannelInvitation-selectedList :contains('john (base.group_user)')",
+            trigger: ".o-discuss-SelectableList-selectedList :contains('john (base.group_user)')",
         },
         {
-            trigger: ".o-discuss-ChannelInvitation-search",
+            trigger: ".o-discuss-SelectableList-search",
             run: "edit unknown_email@test.com",
         },
         {
-            trigger: ".o-discuss-ChannelInvitation-selectable:contains('unknown_email@test.com')",
+            trigger: ".o-discuss-SelectableList-selectable:contains('unknown_email@test.com')",
             run: "click",
         },
         {
-            trigger:
-                ".o-discuss-ChannelInvitation-selectedList :contains('unknown_email@test.com')",
+            trigger: ".o-discuss-SelectableList-selectedList :contains('unknown_email@test.com')",
         },
         {
             trigger: "button:contains(Invite to Group Chat)",

fleetapps/odoo3

addons/mail/static/tests/tours/discuss_invite_by_email_tour.js
@@ -10,13 +10,13 @@ registry.category("web_tour.tours").add("discuss.invite_by_email", {
             run: "click",
         },
         {
-            trigger: ".o-discuss-ChannelInvitation-search[placeholder='Enter name or email']",
+            trigger: ".o-discuss-SelectableList-search[placeholder='Enter name or email']",
             run: "edit john@test.com",
         },
         {
-            trigger: ".o-discuss-ChannelInvitation-selectable:contains('john (base.group_user)')",
+            trigger: ".o-discuss-SelectableList-selectable:contains('john (base.group_user)')",
             async run({ waitFor, click }) {
-                await waitFor(".o-discuss-ChannelInvitation-selectable", {
+                await waitFor(".o-discuss-SelectableList-selectable", {
                     only: true,
                     timeout: 5000,
                 });
@@ -24,20 +24,18 @@ registry.category("web_tour.tours").add("discuss.invite_by_email", {
             },
         },
         {
-            trigger:
-                ".o-discuss-ChannelInvitation-selectedList :contains('john (base.group_user)')",
+            trigger: ".o-discuss-SelectableList-selectedList :contains('john (base.group_user)')",
         },
         {
-            trigger: ".o-discuss-ChannelInvitation-search",
+            trigger: ".o-discuss-SelectableList-search",
             run: "edit unknown_email@test.com",
         },
         {
-            trigger: ".o-discuss-ChannelInvitation-selectable:contains('unknown_email@test.com')",
+            trigger: ".o-discuss-SelectableList-selectable:contains('unknown_email@test.com')",
             run: "click",
         },
         {
-            trigger:
-                ".o-discuss-ChannelInvitation-selectedList :contains('unknown_email@test.com')",
+            trigger: ".o-discuss-SelectableList-selectedList :contains('unknown_email@test.com')",
         },
         {
             trigger: "button:contains(Invite to Group Chat)",