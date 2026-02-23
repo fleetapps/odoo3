2a586c6cb507439bdc792e4819cf28d7fbdbc68f
Vincent Janssen <vijan@odoo.com>
2026-02-23 09:37:56 +0100

    [FIX] project: grant admin access to project stage
    ## Steps to Reproduce:
    1. Ensure the “Project Stage” option is disabled.
    2. Open the form view of any project.
    3. Click the Studio button.
    4. An access error appears, indicating that the current user does not have permission to access stage_id, preventing the project form view from being edited.
    
    ## Expected Behavior After the Commit:
    The admin user should have access to stage_id even when the “Project Stage” option is disabled. This ensures that Studio can be opened without triggering an access rights error.
    
    ## Task :
    
    task-892227

--- addons/project/models/project_project.py
+++ addons/project/models/project_project.py
@@ -158,7 +158,7 @@ class ProjectProject(models.Model):
     collaborator_count = fields.Integer('# Collaborators', compute='_compute_collaborator_count', compute_sudo=True, export_string_translation=False)
 
     # Not `required` since this is an option to enable in project settings.
-    stage_id = fields.Many2one('project.project.stage', string='Stage', ondelete='restrict', groups="project.group_project_stages",
+    stage_id = fields.Many2one('project.project.stage', string='Stage', ondelete='restrict', groups="project.group_project_stages, project.group_project_manager",
         tracking=True, index=True, copy=False, default=_default_stage_id, group_expand='_read_group_expand_full')
     stage_id_color = fields.Integer(string='Stage Color', related="stage_id.color", export_string_translation=False)
     duration_tracking = fields.Json(groups="project.group_project_stages")
