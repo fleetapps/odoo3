7027adac274a9689503bebc2ba9ef18bd9d859c8
Meet Jivani [MEJI] <meji@odoo.com>
2026-02-24 19:24:30 +0530

    [IMP] point_of_sale, **: qr layout on customer display
    **: l10n_in_pos, pos_online_payment
    
    In this commit:
    - Move the payment QR from a popup to the left sidebar of the customer display.
    - Unify online payment and bank QR rendering using the existing QR popup.
    - Remove the online payment popup and use the standard QR popup for consistency.
    
    task-5490801

--- addons/point_of_sale/static/src/customer_display/customer_display.js
+++ addons/point_of_sale/static/src/customer_display/customer_display.js
@@ -1,13 +1,11 @@
 import { useLayoutEffect, useRef } from "@web/owl2/utils";
 import { Component, whenReady } from "@odoo/owl";
 import { OdooLogo } from "@point_of_sale/app/components/odoo_logo/odoo_logo";
-import { useSingleDialog } from "@point_of_sale/customer_display/utils";
 import { MainComponentsContainer } from "@web/core/main_components_container";
 import { session } from "@web/session";
 import { useService } from "@web/core/utils/hooks";
 import { mountComponent } from "@web/env";
 import { BadgeTag } from "@web/core/tags_list/badge_tag";
-import { QRPopup } from "@point_of_sale/app/components/popups/qr_code_popup/qr_code_popup";
 import { useTime } from "@point_of_sale/app/hooks/time_hook";
 
 export class CustomerDisplay extends Component {
@@ -20,7 +18,6 @@ export class CustomerDisplay extends Component {
         this.dialog = useService("dialog");
         this.order = useService("customer_display_data");
         this.time = useTime();
-        const singleDialog = useSingleDialog();
 
         this.scrollableRef = useRef("scrollable");
         useLayoutEffect(() => {
@@ -28,17 +25,13 @@ export class CustomerDisplay extends Component {
                 ?.querySelector(".orderline.selected")
                 ?.scrollIntoView({ behavior: "smooth", block: "start" });
         });
+    }
 
-        useLayoutEffect(
-            (qrPaymentData) => {
-                if (qrPaymentData) {
-                    singleDialog.open(QRPopup, qrPaymentData);
-                } else {
-                    singleDialog.close();
-                }
-            },
-            () => [this.order.qrPaymentData]
-        );
+    get qrPaymentData() {
+        return {
+            ...this.order.qrPaymentData,
+            ...this.order.onlinePaymentData,
+        };
     }
 
     getInternalNotes() {
