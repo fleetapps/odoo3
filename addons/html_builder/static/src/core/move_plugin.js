dzdzdzdz1010/OdooStandard

[FIX] html_builder, *: use hidden_element_predicates to skip siblings
*: website

The function `getNearestSibling` relied on whether an element had style
with `display: none` to tell whether it should be skipped when looking
for the next sibling.

For popup which are hidden when their child is invisible, this was not
working always well. It did work sometimes thanks to the interaction
that attempts to sync the `d-none` class on the popup with the
visibility of its modal.

With this commit, the function `getNearestSibling` skips elements which
matche `hidden_element_predicates`

Steps to reproduce:
- Open website builder
- Add a section (by clicking on a category, do not drag and drop)
- Add a popup (by clicking on a category, do not drag and drop)
- Close the popup
- Add another section (by clicking on a category, do not drag and drop)
- Click on the last section
- Bug: there is the "Move down" arrow of the overlay. Clicking on it
  will move the section "after" the popover (nothing visible)

task-5149984

addons/html_builder/static/src/core/move_plugin.js
@@ -300,7 +300,7 @@ export class MovePlugin extends Plugin {
         const visibleSiblingEls = siblingEls.filter(
             (el) =>
                 !el.classList.contains("o_we_no_overlay") &&
-                window.getComputedStyle(el).display !== "none" &&
+                !this.getResource("hidden_element_predicates").some((p) => p(el)) &&
                 !el.closest(systemNodeSelectors)
         );
         const targetMobileOrder = target.style.order;

fleetapps/odoo3

addons/html_builder/static/src/core/move_plugin.js
@@ -314,7 +314,7 @@ export class MovePlugin extends Plugin {
         const visibleSiblingEls = siblingEls.filter(
             (el) =>
                 !el.classList.contains("o_we_no_overlay") &&
-                window.getComputedStyle(el).display !== "none" &&
+                !this.getResource("is_element_hidden_predicates").some((p) => p(el)) &&
                 !el.closest(systemNodeSelectors)
         );
         const targetMobileOrder = target.style.order;