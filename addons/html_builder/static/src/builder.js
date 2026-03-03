52e89ed360a4142bbfec383023cf3f38569b9f94
Benjamin Vray <bvr@odoo.com>
2026-03-03 08:19:09 +0100

    [IMP]

--- addons/html_builder/static/src/builder.js
+++ addons/html_builder/static/src/builder.js
@@ -43,6 +43,7 @@ export class Builder extends Component {
         overlayRef: { type: Function },
         iframeLoaded: { type: Object },
         isMobile: { type: Boolean },
+        mobileBtnDisabled: { type: Boolean, optional: true },
         Plugins: { type: Array, optional: true },
         // This fragment of config will be passed to the Editor and be
         // available to the plugins in `config`
@@ -59,6 +60,7 @@ export class Builder extends Component {
         themeTabDisplayName: _t("Theme"),
         initialTab: "blocks",
         onlyCustomizeTab: false,
+        mobileBtnDisabled: false,
     };
 
     setup() {
