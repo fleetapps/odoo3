odoo/odoo

[IMP] mrp: enable control over mo confirmation created from procurements
- Currently, manufacturing orders created from replenishment, MPS,or SO
confirmation (procurements) are created in the confirmed state. This restricts
users from selecting or finalizing a specific Bill of Materials when multiple
BoMs exist for the same product. In addition, users may want to confirm mo only
when required components are available, or only a few days before the scheduled
production date, in order to better control production start and reduce
shopfloor noise. With the current behavior, manufacturing orders are confirmed
too early, reducing flexibility in production planning.

- This change adds an option directly on the stock rule to control whether mo
created from procurements are confirmed automatically (except mo without
components) or remain in draft. When the option is enabled, any procurement
triggered by the rule creates the corresponding mo in draft state instead of
confirming it immediately. This gives users better control over when production
starts and allows them to decide when mo should be confirmed based on their
production planning needs.

TaskID-5877241

addons/mrp/models/stock_rule.py
@@ -14,6 +14,10 @@ class StockRule(models.Model):
     action = fields.Selection(selection_add=[
         ('manufacture', 'Manufacture')
     ], ondelete={'manufacture': 'cascade'})
+    draft_production = fields.Boolean(
+        "Draft Production",
+        help="Check this option if you want to create a draft production order on replenishment, instead of a confirmed one."
+    )
 
     def _get_message_dict(self):
         message_dict = super(StockRule, self)._get_message_dict()
@@ -95,6 +99,7 @@ class StockRule(models.Model):
                 procurement_qty = procurement.product_qty
                 batch_size = bom.uom_id._compute_quantity(bom.batch_size, procurement.uom_id) if is_batch_size else procurement_qty
                 vals = rule._prepare_mo_vals(*procurement, bom)
+                procurement.values['draft_production'] = rule.draft_production
                 while procurement.uom_id.compare(procurement_qty, 0) > 0:
                     new_productions_values_by_company[procurement.company_id.id]['values'].append({
                         **vals,
@@ -111,12 +116,13 @@ class StockRule(models.Model):
 
         for company_id in new_productions_values_by_company:
             productions_vals_list = new_productions_values_by_company[company_id]['values']
+            mo_procurements = new_productions_values_by_company[company_id]['procurements']
             # create the MO as SUPERUSER because the current user may not have the rights to do it (mto product launched by a sale for example)
             productions = self.env['mrp.production'].with_user(SUPERUSER_ID).sudo().with_company(company_id).create(productions_vals_list)
-            for mo in productions:
-                if self._should_auto_confirm_procurement_mo(mo):
+            for mo, procurement in zip(productions, mo_procurements):
+                if not procurement.values.get('draft_production') and self._should_auto_confirm_procurement_mo(mo):
                     mo.action_confirm()
-            productions._post_run_manufacture(new_productions_values_by_company[company_id]['procurements'])
+            productions._post_run_manufacture(mo_procurements)
         return True
 
     def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, company_id, values):

fleetapps/odoo3

addons/mrp/models/stock_rule.py
@@ -14,6 +14,10 @@ class StockRule(models.Model):
     action = fields.Selection(selection_add=[
         ('manufacture', 'Manufacture')
     ], ondelete={'manufacture': 'cascade'})
+    allow_draft_production = fields.Boolean(
+        "Draft Production",
+        help="Check this option if you want to create a draft production order on replenishment, instead of a confirmed one."
+    )
 
     def _get_message_dict(self):
         message_dict = super(StockRule, self)._get_message_dict()
@@ -95,6 +99,7 @@ class StockRule(models.Model):
                 procurement_qty = procurement.product_qty
                 batch_size = bom.uom_id._compute_quantity(bom.batch_size, procurement.uom_id) if is_batch_size else procurement_qty
                 vals = rule._prepare_mo_vals(*procurement, bom)
+                procurement.values['allow_draft_production'] = rule.allow_draft_production
                 while procurement.uom_id.compare(procurement_qty, 0) > 0:
                     new_productions_values_by_company[procurement.company_id.id]['values'].append({
                         **vals,
@@ -111,12 +116,13 @@ class StockRule(models.Model):
 
         for company_id in new_productions_values_by_company:
             productions_vals_list = new_productions_values_by_company[company_id]['values']
+            mo_procurements = new_productions_values_by_company[company_id]['procurements']
             # create the MO as SUPERUSER because the current user may not have the rights to do it (mto product launched by a sale for example)
             productions = self.env['mrp.production'].with_user(SUPERUSER_ID).sudo().with_company(company_id).create(productions_vals_list)
-            for mo in productions:
-                if self._should_auto_confirm_procurement_mo(mo):
+            for mo, procurement in zip(productions, mo_procurements):
+                if not procurement.values.get('allow_draft_production') and self._should_auto_confirm_procurement_mo(mo):
                     mo.action_confirm()
-            productions._post_run_manufacture(new_productions_values_by_company[company_id]['procurements'])
+            productions._post_run_manufacture(mo_procurements)
         return True
 
     def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, company_id, values):