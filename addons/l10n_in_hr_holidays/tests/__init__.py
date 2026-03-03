8bc2a68fb7af5826aabab6baf2591b5d8f0ebf92
Yash Vaishnav <yava@odoo.com>
2026-03-03 10:26:43 +0530

    [IMP] l10n_in_hr_holidays: added sandwich days into payslip

--- addons/l10n_in_hr_holidays/tests/__init__.py
+++ addons/l10n_in_hr_holidays/tests/__init__.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_sandwich_leave
+from . import test_sandwich_leave_work_entry
 from . import test_optional_holiday
