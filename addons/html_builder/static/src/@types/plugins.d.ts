dzdzdzdz1010/OdooStandard

wip use has_unsaved-data instead of history no undo

addons/html_builder/static/src/@types/plugins.d.ts
@@ -6,6 +6,7 @@ declare module "plugins" {
     import { CachedModelShared } from "@html_builder/core/cached_model_plugin";
     import { CloneShared, on_cloned_handlers, on_will_clone_handlers } from "@html_builder/core/clone_plugin";
     import { CustomizeTabShared } from "@html_builder/core/customize_tab_plugin";
+    import { DirtMarkShared } from "@html_builder/core/dirt_mark_plugin";
     import { DisableSnippetsShared } from "@html_builder/core/disable_snippets_plugin";
     import { dirt_marks } from "@html_builder/core/dirt_mark_plugin";
     import { dropzone_selector, DropZoneShared, filter_for_sibling_dropzone_predicates } from "@html_builder/core/drop_zone_plugin";
@@ -14,7 +15,7 @@ declare module "plugins" {
     import { OperationShared } from "@html_builder/core/operation_plugin";
     import { get_overlay_buttons, OverlayButtonsShared } from "@html_builder/core/overlay_buttons/overlay_buttons_plugin";
     import { empty_node_predicates, is_unremovable_selector, on_removed_handlers, on_will_remove_handlers, RemoveShared } from "@html_builder/core/remove_plugin";
-    import { after_save_handlers, before_save_handlers, save_handlers, SaveShared } from "@html_builder/core/save_plugin";
+    import { after_save_handlers, before_save_handlers, has_unsaved_data_predicates, save_handlers, SaveShared } from "@html_builder/core/save_plugin";
     import { after_setup_editor_handlers, before_setup_editor_handlers, savable_selectors, SetupEditorShared } from "@html_builder/core/setup_editor_plugin";
     import { target_hide, target_show, VisibilityShared } from "@html_builder/core/visibility_plugin";
     import { default_shape_handlers, image_shape_groups_providers, post_compute_shape_listeners } from "@html_builder/plugins/image/image_shape_option_plugin";
@@ -109,6 +110,7 @@ declare module "plugins" {
         // Predicates
         empty_node_predicates: empty_node_predicates;
         filter_for_sibling_dropzone_predicates: filter_for_sibling_dropzone_predicates;
+        has_unsaved_data_predicates: has_unsaved_data_predicates;
         is_draggable_handlers: is_draggable_handlers;
         keep_overlay_options: keep_overlay_options;
 

fleetapps/odoo3

addons/html_builder/static/src/@types/plugins.d.ts
@@ -15,7 +15,7 @@ declare module "plugins" {
     import { OperationShared } from "@html_builder/core/operation_plugin";
     import { get_overlay_buttons, OverlayButtonsShared, should_show_overlay_buttons_of_ancestor_predicates } from "@html_builder/core/overlay_buttons/overlay_buttons_plugin";
     import { is_node_empty_predicates, is_unremovable_selector, on_removed_handlers, on_will_remove_handlers, RemoveShared } from "@html_builder/core/remove_plugin";
-    import { on_saved_handlers, on_will_save_handlers, on_ready_to_save_document_handlers, SaveShared } from "@html_builder/core/save_plugin";
+    import { has_unsaved_data_predicates, on_saved_handlers, on_will_save_handlers, on_ready_to_save_document_handlers, SaveShared } from "@html_builder/core/save_plugin";
     import { after_setup_editor_overrides, on_will_setup_editor_handlers, savable_selectors, SetupEditorShared } from "@html_builder/core/setup_editor_plugin";
     import { on_target_hidden_handlers, on_target_shown_handlers, VisibilityShared } from "@html_builder/core/visibility_plugin";
     import { default_shape_providers, image_shape_groups_providers, on_shape_computed_handlers } from "@html_builder/plugins/image/image_shape_option_plugin";
@@ -109,6 +109,7 @@ declare module "plugins" {
         // Predicates
         should_keep_overlay_options_predicates: should_keep_overlay_options_predicates;
         should_show_overlay_buttons_of_ancestor_predicates: should_show_overlay_buttons_of_ancestor_predicates;
+        has_unsaved_data_predicates: has_unsaved_data_predicates
         is_draggable_predicates: is_draggable_predicates;
         is_element_in_invisible_panel_predicates: is_element_in_invisible_panel_predicates;
         is_node_empty_predicates: is_node_empty_predicates;