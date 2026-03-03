dzdzdzdz1010/OdooStandard

[IMP] hr: Remove employee_type and use contract_type_id
In this commit, we removed employee_type from hr_version and hr_employee
and we change its usage to contract_type_id.
Also, we renamed the field Contract Type to Employee Type.

task-5430894

addons/hr/models/hr_job.py
@@ -42,7 +42,7 @@ class HrJob(models.Model):
     )
     department_id = fields.Many2one('hr.department', string='Department', check_company=True, tracking=True, index='btree_not_null')
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, tracking=True)
-    contract_type_id = fields.Many2one('hr.contract.type', string='Contract Type', tracking=True)
+    contract_type_id = fields.Many2one('hr.contract.type', string='Employee Type', tracking=True)
 
     _name_company_uniq = models.Constraint(
         'unique(name, company_id, department_id)',

fleetapps/odoo3

addons/hr/models/hr_job.py
@@ -42,7 +42,7 @@ class HrJob(models.Model):
     )
     department_id = fields.Many2one('hr.department', string='Department', check_company=True, tracking=True, index='btree_not_null')
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, tracking=True)
-    contract_type_id = fields.Many2one('hr.contract.type', string='Contract Type', tracking=True)
+    employee_type_id = fields.Many2one('hr.employee.type', string='Employee Type', tracking=True)
 
     _name_company_uniq = models.Constraint(
         'unique(name, company_id, department_id)',