dzdzdzdz1010/OdooStandard

[FIX] html_builder, website: use size observer to know if mobile preview
For elements that are invisible based on the size of the window, the
"Invisible Elements" panel did not update its state when the size of the
window changed (but instead when the mobile preview button changed).

This commit replaces the resource `on_mobile_preview_clicked` by
`device_view_switched_handlers` that is triggered by a plugin that
observes the size of the window.

Steps to reproduce:
- Open website builder
- Add a section
- Make it invisible on mobile
- Reduce the size of the window
- Bug: the section disappears, but is not listed in the invisible
  elements, and it is still the target of the builder

task-5149984

addons/html_builder/static/src/core/disable_snippets_plugin.js
@@ -1,6 +1,5 @@
 import { omit } from "@web/core/utils/objects";
 import { Plugin } from "@html_editor/plugin";
-import { withSequence } from "@html_editor/utils/resource";
 import { selectElements } from "@html_editor/utils/dom_traversal";
 
 /**
@@ -17,7 +16,7 @@ export class DisableSnippetsPlugin extends Plugin {
         on_removed_handlers: this.disableUndroppableSnippets.bind(this),
         post_undo_handlers: this.disableUndroppableSnippets.bind(this),
         post_redo_handlers: this.disableUndroppableSnippets.bind(this),
-        on_mobile_preview_clicked: withSequence(20, this.disableUndroppableSnippets.bind(this)),
+        device_view_switched_handlers: this.disableUndroppableSnippets.bind(this),
     };
 
     setup() {

addons/html_builder/static/src/core/mobile_preview_plugin.js
@@ -1,0 +1,27 @@
+import { Plugin } from "@html_editor/plugin";
+
+/**
+ * @typedef {((args: {isMobileView: boolean}) => ())[]} device_view_switched_handlers
+ * called on switch between previewing a small screen and a large screen
+ */
+
+export class MobilePreviewPlugin extends Plugin {
+    static id = "mobilePreview";
+
+    setup() {
+        this.isMobileView = this.config.isMobileView(this.editable);
+        this.observer = new ResizeObserver(() => {
+            const isMobileView = this.config.isMobileView(this.editable);
+            if (this.isMobileView !== isMobileView) {
+                this.isMobileView = isMobileView;
+                this.dispatchTo("device_view_switched_handlers", { isMobileView });
+            }
+        });
+        this.observer.observe(this.editable);
+    }
+
+    destroy() {
+        super.destroy();
+        this.observer.disconnect();
+    }
+}

addons/html_builder/static/src/core/overlay_buttons/overlay_buttons_plugin.js
@@ -3,7 +3,6 @@ import { reactive } from "@odoo/owl";
 import { throttleForAnimation } from "@web/core/utils/timing";
 import { getScrollingElement, getScrollingTarget } from "@web/core/utils/scrolling";
 import { OverlayButtons } from "./overlay_buttons";
-import { withSequence } from "@html_editor/utils/resource";
 
 /** @typedef {import("@html_builder/core/builder_options_plugin").BuilderButtonDescriptor} BuilderButtonDescriptor */
 /**
@@ -34,7 +33,7 @@ export class OverlayButtonsPlugin extends Plugin {
         selection_leave_handlers: this.showOverlayButtonsUi.bind(this),
         step_added_handlers: this.refreshButtons.bind(this),
         change_current_options_containers_listeners: this.addOverlayButtons.bind(this),
-        on_mobile_preview_clicked: withSequence(20, this.refreshButtons.bind(this)),
+        device_view_switched_handlers: this.refreshButtons.bind(this),
     };
 
     setup() {

addons/html_builder/static/src/core/visibility_plugin.js
@@ -30,7 +30,7 @@ export class VisibilityPlugin extends Plugin {
     ];
     /** @type {import("plugins").BuilderResources} */
     resources = {
-        on_mobile_preview_clicked: withSequence(10, this.onMobilePreviewClicked.bind(this)),
+        device_view_switched_handlers: withSequence(5, this.onMobilePreviewClicked.bind(this)),
         system_attributes: ["data-invisible"],
         system_classes: ["o_snippet_override_invisible"],
         clean_for_save_handlers: this.cleanForSaveVisibility.bind(this),

fleetapps/odoo3

addons/html_builder/static/src/core/disable_snippets_plugin.js
@@ -1,6 +1,5 @@
 import { omit } from "@web/core/utils/objects";
 import { Plugin } from "@html_editor/plugin";
-import { withSequence } from "@html_editor/utils/resource";
 import { selectElements } from "@html_editor/utils/dom_traversal";
 
 /**
@@ -17,7 +16,7 @@ export class DisableSnippetsPlugin extends Plugin {
         on_removed_handlers: this.disableUndroppableSnippets.bind(this),
         on_undone_handlers: this.disableUndroppableSnippets.bind(this),
         on_redone_handlers: this.disableUndroppableSnippets.bind(this),
-        on_mobile_preview_clicked: withSequence(20, this.disableUndroppableSnippets.bind(this)),
+        on_device_view_switched_handlers: this.disableUndroppableSnippets.bind(this),
     };
 
     setup() {

addons/html_builder/static/src/core/mobile_preview_plugin.js
@@ -1,0 +1,27 @@
+import { Plugin } from "@html_editor/plugin";
+
+/**
+ * @typedef {((args: {isMobileView: boolean}) => ())[]} on_device_view_switched_handlers
+ * called on switch between previewing a small screen and a large screen
+ */
+
+export class MobilePreviewPlugin extends Plugin {
+    static id = "mobilePreview";
+
+    setup() {
+        this.isMobileView = this.config.isMobileView(this.editable);
+        this.observer = new ResizeObserver(() => {
+            const isMobileView = this.config.isMobileView(this.editable);
+            if (this.isMobileView !== isMobileView) {
+                this.isMobileView = isMobileView;
+                this.trigger("on_device_view_switched_handlers", { isMobileView });
+            }
+        });
+        this.observer.observe(this.editable);
+    }
+
+    destroy() {
+        super.destroy();
+        this.observer.disconnect();
+    }
+}

addons/html_builder/static/src/core/overlay_buttons/overlay_buttons_plugin.js
@@ -3,7 +3,6 @@ import { Plugin } from "@html_editor/plugin";
 import { throttleForAnimation } from "@web/core/utils/timing";
 import { getScrollingElement, getScrollingTarget } from "@web/core/utils/scrolling";
 import { OverlayButtons } from "./overlay_buttons";
-import { withSequence } from "@html_editor/utils/resource";
 
 /** @typedef {import("@html_builder/core/builder_options_plugin").BuilderButtonDescriptor} BuilderButtonDescriptor */
 /**
@@ -35,7 +34,7 @@ export class OverlayButtonsPlugin extends Plugin {
         on_selection_leave_handlers: this.showOverlayButtonsUi.bind(this),
         on_step_added_handlers: this.refreshButtons.bind(this),
         on_current_options_containers_changed_handlers: this.addOverlayButtons.bind(this),
-        on_mobile_preview_clicked: withSequence(20, this.refreshButtons.bind(this)),
+        on_device_view_switched_handlers: this.refreshButtons.bind(this),
     };
 
     setup() {

addons/html_builder/static/src/core/visibility_plugin.js
@@ -30,7 +30,7 @@ export class VisibilityPlugin extends Plugin {
     ];
     /** @type {import("plugins").BuilderResources} */
     resources = {
-        on_mobile_preview_clicked: withSequence(10, this.onMobilePreviewClicked.bind(this)),
+        on_device_view_switched_handlers: withSequence(5, this.onMobilePreviewClicked.bind(this)),
         system_attributes: ["data-invisible"],
         system_classes: ["o_snippet_override_invisible"],
         clean_for_save_processors: this.cleanForSaveVisibility.bind(this),