295684cb1bbde0b82842fd67e31caefee9d6ed83
Antoine Vandevenne (anv) <anv@odoo.com>
2026-03-04 15:34:35 +0100

    review

--- addons/sale_gelato/tests/test_product_template.py
+++ addons/sale_gelato/tests/test_product_template.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from odoo.tests import tagged
 
 from odoo.addons.sale.tests.common import SaleCommon
@@ -32,7 +33,6 @@ class TestGelatoProductTemplate(SaleCommon):
                 }
             ],
         }
-
         cls.template_data_two_variants = dict(
             cls.template_data_one_variant,
             variants=[
@@ -47,7 +47,6 @@ class TestGelatoProductTemplate(SaleCommon):
                 },
             ],
         )
-
         cls.template_data_three_variants = dict(
             cls.template_data_two_variants,
             variants=[
@@ -63,57 +62,77 @@ class TestGelatoProductTemplate(SaleCommon):
             ],
         )
 
-    def create_attribute(self, name):
+    def _create_attribute(self, name):
         return self.env['product.attribute'].create({'name': name, 'create_variant': 'always'})
 
+    def test_creating_attributes_sets_product_uid_on_template(self):
+        """Test that the product UID is assigned to the template if there is only one variant."""
+        self.gelato_template._create_attributes_from_gelato_info(self.template_data_one_variant)
+        self.assertEqual(self.gelato_template.gelato_product_uid, 'm_orange_tshirt_uid')
+
     def test_synchronizing_template_creates_product_variants(self):
-        """Test that correct amount of product variants are created."""
+        """Test that the correct amount of product variants are created."""
         self.gelato_template._create_attributes_from_gelato_info(self.template_data_three_variants)
         self.assertEqual(self.gelato_template.product_variant_count, 3)
 
-    def test_synchronizing_template_creates_and_adds_attributes(self):
-        """Test that non-existing attributes are created and added on the template."""
+    def test_synchronizing_template_creates_missing_attributes(self):
+        """Test that missing attributes are created."""
         self.assertEqual(
             self.env['product.attribute'].search_count([('name', 'in', ['Color', 'Size'])]),
             0,
-            "Color and Size attributes should not exist before synchronization.",
+            msg="Color and Size attributes should not exist before synchronization.",
         )
-
         self.gelato_template._create_attributes_from_gelato_info(self.template_data_two_variants)
-        added_attributes = self.gelato_template.attribute_line_ids.attribute_id.mapped('name')
-        self.assertEqual(added_attributes, ['Size', 'Color'])
-
-    def test_synchronizing_template_adds_existing_attributes(self):
-        """Test that already existing attribute is added on template instead of creating new one."""
-        existing_color_attribute = self.create_attribute(name='Color')
-
-        self.gelato_template._create_attributes_from_gelato_info(self.template_data_two_variants)
-        template_color_attribute = self.gelato_template.attribute_line_ids[0].attribute_id
         self.assertEqual(
-            template_color_attribute,
-            existing_color_attribute,
-            msg="Existing attribute should be used instead of creating new one.",
+            self.env['product.attribute'].search_count([('name', 'in', ['Color', 'Size'])]), 2
         )
 
-    def test_synchronizing_template_creates_and_adds_attribute_values(self):
-        """Test that non-existing attribute values are created and added on the template."""
+    def test_synchronizing_template_assigns_new_attributes(self):
+        """Test that newly created attributes are assigned to the template."""
+        self.gelato_template._create_attributes_from_gelato_info(self.template_data_two_variants)
+        added_attribute_names = self.gelato_template.attribute_line_ids.attribute_id.mapped('name')
+        self.assertEqual(added_attribute_names, ['Size', 'Color'])
+
+    def test_synchronizing_template_assigns_existing_attributes(self):
+        """Test that already existing attributes are assigned to the template instead of creating
+        new ones."""
+        existing_color_attribute = self._create_attribute(name='Color')
+        self.gelato_template._create_attributes_from_gelato_info(self.template_data_two_variants)
+        assigned_template_attribute = self.gelato_template.attribute_line_ids[0].attribute_id
+        self.assertEqual(
+            assigned_template_attribute,
+            existing_color_attribute,
+            msg="Existing attributes should be used instead of creating new ones.",
+        )
+
+    def test_synchronizing_template_creates_missing_attribute_values(self):
+        """Test that missing attribute values are created."""
         self.assertEqual(
             self.env['product.attribute.value'].search_count([('name', 'in', ['Orange', 'Red'])]),
             0,
             "Red and Orange attribute values should exist before synchronization.",
         )
+        self.gelato_template._create_attributes_from_gelato_info(self.template_data_three_variants)
+        self.assertEqual(
+            self.env['product.attribute.value'].search_count([('name', 'in', ['Orange', 'Red'])]),
+            2,
+            "Red and Orange attribute values should exist before synchronization.",
+        )
 
+    def test_synchronizing_template_assigns_new_attribute_values(self):
+        """Test that newly created attribute values are assigned to the template."""
         self.gelato_template._create_attributes_from_gelato_info(self.template_data_three_variants)
         self.assertEqual(
             self.gelato_template.attribute_line_ids.value_ids.mapped('name'),
             ['M', 'L', 'Orange', 'Red'],
         )
 
-    def test_synchronizing_template_adds_existing_attribute_values_on_template(self):
-        """Test that if attribute value already exists, it's added to the template."""
-        color_attribute = self.create_attribute(name='Color')
+    def test_synchronizing_template_assigns_existing_attribute_values(self):
+        """Test that already existing attribute values are assigned to the template instead of
+        creating new ones."""
+        color_attribute = self._create_attribute(name="Color")
         orange_attribute_value = self.env['product.attribute.value'].create({
-            'name': 'Orange',
+            'name': "Orange",
             'attribute_id': color_attribute.id,
         })
         self.gelato_template._create_attributes_from_gelato_info(self.template_data_two_variants)
@@ -121,13 +140,8 @@ class TestGelatoProductTemplate(SaleCommon):
         self.assertEqual(
             variant_attribute_value,
             orange_attribute_value,
-            msg="Existing attribute value should be assigned to the product.",
+            msg="Existing attribute values should be used instead of creating new ones.",
         )
-
-    def test_creating_attributes_sets_product_uid_on_template(self):
-        """Test that product UID is assigned to the template if there is only one variant."""
-        self.gelato_template._create_attributes_from_gelato_info(self.template_data_one_variant)
-        self.assertEqual(self.gelato_template.gelato_product_uid, 'm_orange_tshirt_uid')
 
     def test_creating_attributes_sets_product_uids_on_variants(self):
         """Test that all created variants are assigned a Gelato product UID."""
@@ -138,6 +152,6 @@ class TestGelatoProductTemplate(SaleCommon):
         )
 
     def test_creating_print_images_saves_front_as_default(self):
-        """Test that 'front' or '1' image placement names are saved as 'default'."""
+        """Test that 'front' and '1' image placement names are saved as 'default'."""
         self.gelato_template._create_print_images_from_gelato_info(self.template_data_one_variant)
-        self.assertEqual(self.gelato_template.gelato_image_ids.mapped('name'), ['back', 'default'])
+        self.assertEqual(self.gelato_template.gelato_image_ids.mapped('name')[1], 'default')
--- addons/sale_gelato/tests/test_sale_order.py
+++ addons/sale_gelato/tests/test_sale_order.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from unittest.mock import patch
+
 from odoo import Command
 from odoo.exceptions import ValidationError
 from odoo.tests import tagged
@@ -16,7 +18,6 @@ class TestGelatoSaleOrder(SaleCommon):
             'name': 'Test Gelato Product',
             'gelato_product_uid': 'dummy_uid',
         })
-
         cls.gelato_order = cls.env['sale.order'].create({
             'partner_id': cls.partner.id,
             'order_line': [
@@ -24,21 +25,37 @@ class TestGelatoSaleOrder(SaleCommon):
             ],
         })
 
-    def test_add_non_gelato_service_product_to_gelato_order(self):
-        """Test that adding a non-gelato service product to Gelato order is possible."""
+    def test_allow_adding_generic_service_product_to_gelato_order(self):
+        """Test that adding a non-gelato, service product to a Gelato order is allowed."""
         self.env['sale.order.line'].create({
             'product_id': self.service_product.id,
             'order_id': self.gelato_order.id,
         })
-
         self.assertEqual(len(self.gelato_order.order_line.ids), 2)
 
-    def test_add_non_gelato_goods_product_to_gelato_order(self):
+    def test_allow_modifying_generic_service_product_on_gelato_order(self):
+        """Test that adding a non-gelato, service product to a Gelato order is allowed."""
+        order_line = self.env['sale.order.line'].create({
+            'product_id': self.service_product.id,
+            'order_id': self.gelato_order.id,
+        })
+        service_product_2 = self.service_product.copy()
+        order_line.product_id = service_product_2.id
+        self.assertEqual(order_line.product_id, service_product_2)
+
+    def test_prevent_adding_generic_goods_product_to_gelato_order(self):
         """Test that adding a non-gelato goods product to Gelato order is not possible."""
-        with self.assertRaises(
-            ValidationError, msg="It shouldn't be possible to mix gelato and non-gelato products"
-        ):
+        with self.assertRaises(ValidationError):
             self.env['sale.order.line'].create({
                 'product_id': self.product.id,
                 'order_id': self.gelato_order.id,
             })
+
+    def test_prevent_modifying_generic_service_product_to_goods_product_on_gelato_order(self):
+        """Test that adding a non-gelato goods product to Gelato order is not possible."""
+        order_line = self.env['sale.order.line'].create({
+            'product_id': self.service_product.id,
+            'order_id': self.gelato_order.id,
+        })
+        with self.assertRaises(ValidationError):
+            order_line.product_id = self.product.id
