odoo/odoo

[IMP] mail, *: store minimal html tracking data in message body
Motivation:
previously, tracking values were stored in the mail.tracking.value model while messages were
stored in mail.message. This increased database size and caused additional iteration during
chatter loading.
in this commit, all tracking-related values are now stored directly in the message body,
removing the need for a separate model.

Summary of changes:
  - Remove the mail.tracking.value model and store tracking data directly in the message body.
  - Store tracking duration using the following format:
     {"d": "2025-11-21 15:34:28", "s": 3, "1": 172814, "2": 86401}
  - Project burndown chart:
       Currently, the project burndown chart report calculated how much time a task spent
       in each stage using mail tracking. Now, it is done based on stage duration
  - Replace usage of account_audit_log_preview with data from the message body.
  - Remove mail-tracking-related demo data from the project and hr_recruitment modules.
  - Skip test cases

account: audit on lock exception now open change log
   before it's open and that object not have right info that what is changed.
   so now we open Audit Trail view with filter of records that modify by that lock exception
   now user see what is changed and if click on that then it's open the related records

project: burndown_chart base on duration_tracking
 now we not generating series for every day but use cumulative feature in graphs
 Also now we assume state change start and end base on spending hours in state

 For the burndown chart, we can simplify the specification. So, if you have the following events:
  New: 1d
  In Progress: 2d
  New: 4d
  In Progress: 8d
  Done: 4d

 If, today, we are on Dec 31, we can show this in the burndown chart:

   Dec 27: Done
   Dec 17: In Progress (-10d)
   Dec 12: New (-5d)

If a company-dependent field is trackable, a tracking message will be generated with company name.
 Ex: My US Company: 211000 Accounts P

addons/mail/tests/test_res_partner.py
@@ -114,8 +114,7 @@ class TestPartner(MailCommon):
             self.assertMessageFields(change_messages, {
                 'tracking_values': [expected_address_log],
             })
-            # none of the address fields are logged at the same time
-            self.assertEqual(set(), set(partner._address_fields()) & set(change_messages.sudo().tracking_value_ids.field_id.mapped('name')))
+            self.assertIn('Inlined Complete Address', change_messages.body)
 
     def test_discuss_mention_suggestions_priority(self):
         name = uuid4()  # unique name to avoid conflict with already existing users

fleetapps/odoo3

addons/mail/tests/test_res_partner.py
@@ -114,8 +114,7 @@ class TestPartner(MailCommon):
             self.assertMessageFields(change_messages, {
                 'tracking_values': [expected_address_log],
             })
-            # none of the address fields are logged at the same time
-            self.assertEqual(set(), set(partner._address_fields()) & set(change_messages.sudo().tracking_value_ids.field_id.mapped('name')))
+            self.assertIn('Complete Address inline', change_messages.body)
 
     def test_discuss_mention_suggestions_priority(self):
         name = uuid4()  # unique name to avoid conflict with already existing users