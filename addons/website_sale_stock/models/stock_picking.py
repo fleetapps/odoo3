dzdzdzdz1010/OdooStandard

[IMP] *: use new pickup location selector in backend
*: delivery, sale, stock_delivery, website_sale

This commit introduces several changes and improvements to pickup point
delivery feature.

** Behavior before this commit:
- Users could choose a pickup address on checkout, if a proper delivery
  method is enabled.
- Data of this address is attached to the sales order in a json field
  and the delivery address is set to the customer's address as in normal
  delivery.
- On confirming the sale order, a new address for the pickup point is
  created as a child to the customer's contact. Its data is retrieved
  from the above mentioned json field.
- On confirming the delivery order, the address data to be passed to the
  shipping provider to process the shipment is retrieved from the
  related sales order (i.e. pickup delivery orders _must_ have a related
  sales order).
- It was not possible to change the pickup address from the backend
  after the user confirms the order.

** Behavior after this commit:
- The json field is removed from the sales order.
- When the user confirms an order with a pickup delivery, a new address
  is created with the point data (if there's no existing address with the same data).
- A new json field is added to `res.partner` to capture the info needed
  by the shipping provider later when processing the shipment.
- Pickup address can be changed from sales/delivery order using the new
  location selector (introduced in #156382) which is currently
  used in eCommerce.

Task-3645144

addons/website_sale_stock/models/stock_picking.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+import json
+
+from odoo import _, fields, models
+from odoo.exceptions import UserError
 
 
 class StockPicking(models.Model):
@@ -10,3 +13,41 @@ class StockPicking(models.Model):
     website_id = fields.Many2one('website', related='sale_id.website_id', string='Website',
                                  help='Website where this order has been placed, for eCommerce orders.',
                                  store=True, readonly=True)
+    is_pickup_delivery = fields.Boolean(related='carrier_id.is_pickup')
+
+    def write(self, vals):
+        """ Override to reset the partner_id if the carrier is changed from a pickup to a non-pickup one. """
+        if 'carrier_id' in vals:
+            new_carrier = self.env['delivery.carrier'].browse(vals['carrier_id'])
+            if not new_carrier.is_pickup and any(picking.carrier_id.is_pickup for picking in self):
+                pickings_with_pickup = self.filtered(lambda so: so.carrier_id.is_pickup)
+                for picking in pickings_with_pickup:
+                    picking.partner_id = False
+        return super().write(vals)
+
+    def action_confirm(self):
+        """ Override to prevent confirming a picking with a pickup delivery method with a wrong address.
+
+        :raises UserError: if the picking is a pickup delivery and no pickup address is selected or the selected address belongs to another delivery method.
+        """
+        if any(picking.is_pickup_delivery and picking.partner_id.pickup_delivery_carrier_id.id != picking.carrier_id.id for picking in self):
+            raise UserError(_("You must select a pickup address with this delivery method."))
+        return super().action_confirm()
+
+    def button_validate(self):
+        """ Override to prevent validating a picking with a pickup delivery method with a wrong address.
+
+        :raises UserError: if the picking is a pickup delivery and no pickup address is selected or the selected address belongs to another delivery method.
+        """
+        if any(picking.is_pickup_delivery and picking.partner_id.pickup_delivery_carrier_id.id != picking.carrier_id.id for picking in self):
+            raise UserError(_("You must select a pickup address with this delivery method."))
+        return super().button_validate()
+
+    def set_pickup_location(self, pickup_location_data):
+        """ Set the pickup location on the current record. """
+        self.ensure_one()
+        if self.carrier_id.is_pickup:
+            pickup_location_data_json = json.loads(pickup_location_data)
+            parent_location = self.partner_id.parent_id if self.partner_id.pickup_delivery_carrier_id else self.partner_id
+            address = self.env['res.partner']._address_from_json(pickup_location_data_json, parent_location, pickup_delivery_carrier_id=self.carrier_id.id)
+            self.partner_id = address or self.partner_id

fleetapps/odoo3

addons/website_sale_stock/models/stock_picking.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+import json
+
+from odoo import _, fields, models
+from odoo.exceptions import UserError
 
 
 class StockPicking(models.Model):
@@ -10,3 +13,41 @@ class StockPicking(models.Model):
     website_id = fields.Many2one('website', related='sale_id.website_id', string='Website',
                                  help='Website where this order has been placed, for eCommerce orders.',
                                  store=True, readonly=True)
+    is_pickup_required = fields.Boolean(related='carrier_id.support_pickup_locations')
+
+    def write(self, vals):
+        """ Override to reset the partner_id if the carrier is changed from a pickup to a non-pickup one. """
+        if 'carrier_id' in vals:
+            new_carrier = self.env['delivery.carrier'].browse(vals['carrier_id'])
+            if not new_carrier.support_pickup_locations and any(picking.carrier_id.support_pickup_locations for picking in self):
+                pickings_with_pickup = self.filtered(lambda so: so.carrier_id.support_pickup_locations)
+                for picking in pickings_with_pickup:
+                    picking.partner_id = False
+        return super().write(vals)
+
+    def action_confirm(self):
+        """ Override to prevent confirming a picking with a pickup delivery method with a wrong address.
+
+        :raises UserError: if the picking is a pickup delivery and no pickup address is selected or the selected address belongs to another delivery method.
+        """
+        if any(picking.is_pickup_required and picking.partner_id.pickup_delivery_method_id.id != picking.carrier_id.id for picking in self):
+            raise UserError(_("You must select a pickup address with this delivery method."))
+        return super().action_confirm()
+
+    def button_validate(self):
+        """ Override to prevent validating a picking with a pickup delivery method with a wrong address.
+
+        :raises UserError: if the picking is a pickup delivery and no pickup address is selected or the selected address belongs to another delivery method.
+        """
+        if any(picking.is_pickup_required and picking.partner_id.pickup_delivery_method_id.id != picking.carrier_id.id for picking in self):
+            raise UserError(_("You must select a pickup address with this delivery method."))
+        return super().button_validate()
+
+    def set_pickup_location(self, pickup_location_data):
+        """ Set the pickup location on the current record. """
+        self.ensure_one()
+        if self.is_pickup_required:
+            pickup_location_data_json = json.loads(pickup_location_data)
+            parent_location = self.partner_id.parent_id if self.partner_id.pickup_delivery_method_id else self.partner_id
+            address = self.env['res.partner']._address_from_json(pickup_location_data_json, parent_location, pickup_delivery_method_id=self.carrier_id.id)
+            self.partner_id = address or self.partner_id