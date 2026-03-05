odoo/odoo

[IMP] hr_attendance: Provision for Single Check-In System
After:

- This commit introduces a new single check-in attendance system.
- It works with the auto-checkout feature already available in attendance.
- If single check-in is enabled, employees will not be able to check-in
  more than once per day; checkout will be handled automatically by
  the auto-checkout feature.

task-4848733

addons/hr_attendance/static/src/components/greetings/greetings.js
@@ -18,6 +18,7 @@ export class KioskGreetings extends Component {
         this.attendance = this.props.employeeData.attendance;
         this.check_in_time = this.formatDateTime(this.attendance.check_in && deserializeDateTime(this.attendance.check_in));
         this.check_out_time = this.formatDateTime(this.attendance.check_out && deserializeDateTime(this.attendance.check_out));
+        this.single_checkin_enabled = this.props.employeeData.single_checkin_enabled;
         this.kiosk_delay = setTimeout(() => {
             this.props.kioskReturn(true)
         }, this.props.employeeData.kiosk_delay)

fleetapps/odoo3

addons/hr_attendance/static/src/components/greetings/greetings.js
@@ -18,6 +18,7 @@ export class KioskGreetings extends Component {
         this.attendance = this.props.employeeData.attendance;
         this.check_in_time = this.formatDateTime(this.attendance.check_in && deserializeDateTime(this.attendance.check_in));
         this.check_out_time = this.formatDateTime(this.attendance.check_out && deserializeDateTime(this.attendance.check_out));
+        this.isEmployeeSingleCheckIn = this.props.employeeData.is_employee_single_checkin;
         this.kiosk_delay = setTimeout(() => {
             this.props.kioskReturn(true)
         }, this.props.employeeData.kiosk_delay)