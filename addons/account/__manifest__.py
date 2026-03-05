odoo/odoo

[IMP] account: Remove `sending_data` from `account.move`
The sending_data column is used only for background processing and is not related to account.move attributes or business data. The account.move table is probably the second largest table in any Odoo database, and the sending_data column is almost always None and has no real functional value.

Move the background processing logic to a dedicated event_processor and remove the sending_data column. 

task-5933703

addons/account/__manifest__.py
@@ -67,6 +67,7 @@ You could use this simplified accounting in case you work with an (external) acc
         'views/account_portal_templates.xml',
         'views/report_payment_receipt_templates.xml',
         'data/service_cron.xml',
+        'data/account_event_process_cron.xml',
         'views/account_incoterms_view.xml',
         'data/account_incoterms_data.xml',
         'views/digest_views.xml',

addons/l10n_it_edi/tests/test_account_move_send.py
@@ -47,7 +47,7 @@ class TestItAccountMoveSend(TestItEdi, TestAccountMoveSendCommon):
             self.env['account.move.send']._generate_and_send_invoices(invoice1 + invoice2)
 
         # Asserts
-        self.assertEqual((invoice1 + invoice2).mapped('sending_data'), [False, False])
+        self.assertTrue(all(not self.env['account.event.process'].get_record_active_event_data(inv) for inv in (invoice1 + invoice2)))
         self.assertEqual(1, len(self.get_attachments(invoice1.id)))
         self.assertTrue(invoice1.invoice_pdf_report_id)
         self.assertFalse(invoice1.l10n_it_edi_attachment_file)
@@ -73,7 +73,7 @@ class TestItAccountMoveSend(TestItEdi, TestAccountMoveSendCommon):
             self.env['account.move.send']._generate_and_send_invoices(invoice1 + invoice2, sending_methods=['email'])
 
         # Asserts
-        self.assertEqual((invoice1 + invoice2).mapped('sending_data'), [False, False])
+        self.assertTrue(all(not self.env['account.event.process'].get_record_active_event_data(inv) for inv in (invoice1 + invoice2)))
         self.assertEqual(2, len(self.get_attachments(invoice1.id)))
         self.assertTrue(invoice1.invoice_pdf_report_id)
         self.assertTrue(invoice1.l10n_it_edi_attachment_file)
@@ -204,7 +204,15 @@ class TestItAccountMoveSend(TestItEdi, TestAccountMoveSendCommon):
 
     def test_l10n_it_edi_send_from_cron(self):
         invoices = self._create_invoice_it() + self._create_invoice_it()
-        invoices.sending_data = {'author_user_id': self.env.user.id, 'author_partner_id': self.env.user.partner_id.id}
+        self.env['account.event.process'].schedule_events(
+            [
+                {
+                    'record': i,
+                    'event_code': 'action_move_send',
+                    'data': {'author_user_id': self.env.user.id, 'author_partner_id': self.env.user.partner_id.id},
+                } for i in invoices
+            ],
+        )
         self.generate_l10n_it_edi_send_attachments(invoices, from_cron=True)
 
         success = {'id_transaction': "SDI ID 1", 'signed': False, 'signed_data': False}

fleetapps/odoo3

addons/account/__manifest__.py
@@ -66,6 +66,7 @@ You could use this simplified accounting in case you work with an (external) acc
         'views/account_portal_templates.xml',
         'views/report_payment_receipt_templates.xml',
         'data/service_cron.xml',
+        'data/account_move_event_process_cron.xml',
         'views/account_incoterms_view.xml',
         'data/account_incoterms_data.xml',
         'views/digest_views.xml',

addons/l10n_it_edi/tests/test_account_move_send.py
@@ -47,7 +47,10 @@ class TestItAccountMoveSend(TestItEdi, TestAccountMoveSendCommon):
             self.env['account.move.send']._generate_and_send_invoices(invoice1 + invoice2)
 
         # Asserts
-        self.assertEqual((invoice1 + invoice2).mapped('sending_data'), [False, False])
+        self.assertTrue(all(not self.env['account.move.event.process'].get_move_active_event_data(
+            inv,
+            'account_move_send',
+        ) for inv in (invoice1 + invoice2)))
         self.assertEqual(1, len(self.get_attachments(invoice1.id)))
         self.assertTrue(invoice1.invoice_pdf_report_id)
         self.assertFalse(invoice1.l10n_it_edi_attachment_file)
@@ -73,7 +76,10 @@ class TestItAccountMoveSend(TestItEdi, TestAccountMoveSendCommon):
             self.env['account.move.send']._generate_and_send_invoices(invoice1 + invoice2, sending_methods=['email'])
 
         # Asserts
-        self.assertEqual((invoice1 + invoice2).mapped('sending_data'), [False, False])
+        self.assertTrue(all(not self.env['account.move.event.process'].get_move_active_event_data(
+            inv,
+            'account_move_send',
+        ) for inv in (invoice1 + invoice2)))
         self.assertEqual(2, len(self.get_attachments(invoice1.id)))
         self.assertTrue(invoice1.invoice_pdf_report_id)
         self.assertTrue(invoice1.l10n_it_edi_attachment_file)
@@ -204,7 +210,15 @@ class TestItAccountMoveSend(TestItEdi, TestAccountMoveSendCommon):
 
     def test_l10n_it_edi_send_from_cron(self):
         invoices = self._create_invoice_it() + self._create_invoice_it()
-        invoices.sending_data = {'author_user_id': self.env.user.id, 'author_partner_id': self.env.user.partner_id.id}
+        self.env['account.move.event.process'].schedule_events(
+            [
+                {
+                    'move': i,
+                    'event_code': 'account_move_send',
+                    'data': {'author_user_id': self.env.user.id, 'author_partner_id': self.env.user.partner_id.id},
+                } for i in invoices
+            ],
+        )
         self.generate_l10n_it_edi_send_attachments(invoices, from_cron=True)
 
         success = {'id_transaction': "SDI ID 1", 'signed': False, 'signed_data': False}